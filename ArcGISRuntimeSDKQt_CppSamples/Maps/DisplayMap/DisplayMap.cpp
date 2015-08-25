7df0cca6cdb22877e8c6cdd4a11802c2d117ec19
Lucas Danzinger <LDanzinger@esri.com>
2015-08-25 16:15:09 -0700

    adding display a map c++ sample

---
+++ ArcGISRuntimeSDKQt_CppSamples/Maps/DisplayMap/DisplayMap.cpp
@@ -0,0 +1,42 @@
+// Copyright 2015 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "DisplayMap.h"
+#include "Map.h"
+#include "MapView.h"
+#include <QDir>
+#include <QUrl>
+#include <QVBoxLayout>
+
+using namespace Esri::ArcGISRuntime;
+
+DisplayMap::DisplayMap(QWidget* parent) :
+  QWidget(parent),
+  m_map(nullptr),
+  m_mapView(nullptr)
+{    
+    // Create a map using the Imagery with labels basemap
+    m_map = new Map(Basemap::imageryWithLabels(this), this);
+
+    // Create a map view, and pass in the map
+    m_mapView = new MapView(m_map, this);
+
+    // Set up the UI
+    QVBoxLayout *vBoxLayout = new QVBoxLayout();
+    vBoxLayout->addWidget(m_mapView);
+    setLayout(vBoxLayout);
+}
+
+DisplayMap::~DisplayMap()
+{
+}
