1aa7ffb8ae6f3f77ac3612d68424c823e4e47fa5
kous3106 <khajra@esri.com>
2015-10-19 11:35:21 -0700

    Initial check-in of the selection sample

---
+++ ArcGISRuntimeSDKQt_CppSamples/Features/FeatureLayer_Selection/Selection.h
@@ -0,0 +1,47 @@
+// Copyright 2015 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SELECTION_H
+#define SELECTION_H
+
+#include "Map.h"
+#include "MapView.h"
+#include "Viewpoint.h"
+#include "FeatureLayer.h"
+#include "ServiceFeatureTable.h"
+#include <QPushButton>
+#include <QLineEdit>
+
+class Selection : public QWidget
+{
+  Q_OBJECT
+
+public:
+  explicit Selection(QWidget* parent = 0);
+  virtual ~Selection();
+
+public slots:
+  void onQueryClicked();
+
+private:
+  Esri::ArcGISRuntime::Map* m_map;
+  Esri::ArcGISRuntime::MapView* m_mapView;
+  Esri::ArcGISRuntime::FeatureLayer* m_featureLayer;
+  Esri::ArcGISRuntime::ServiceFeatureTable* m_featureTable;
+  QLineEdit* m_state;
+  QPushButton* m_queryButton;
+
+  void createUi();
+};
+
+#endif // SELECTION_H
