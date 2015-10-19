1baed6edbfd1d57c9b10328c7bfe7bd89b7340b9
Michael Tims <mtims@esri.com>
2015-10-19 10:15:30 -0700

    Updating to use ANGLE for Windows

--- ArcGISRuntimeSDKQt_QMLSamples/Maps/ChangeViewpoint/main.cpp
+++ ArcGISRuntimeSDKQt_QMLSamples/Maps/ChangeViewpoint/main.cpp
@@ -17,6 +17,11 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
+    
+#ifdef Q_OS_WIN
+    // Force usage of OpenGL ES through ANGLE on Windows
+    QCoreApplication::setAttribute(Qt::AA_UseOpenGLES);
+#endif
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/Samples/Maps/ChangeViewpoint/main.qml")));
