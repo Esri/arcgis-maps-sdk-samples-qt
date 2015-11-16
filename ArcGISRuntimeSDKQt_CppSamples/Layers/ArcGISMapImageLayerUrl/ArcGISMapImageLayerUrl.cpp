55fce2da07207180e03b5b2e84b810a0aa58bf24
Lucas Danzinger <LDanzinger@esri.com>
2015-11-16 13:16:55 -0800

    adding map image layer sample

---
+++ ArcGISRuntimeSDKQt_CppSamples/Layers/ArcGISMapImageLayerUrl/ArcGISMapImageLayerUrl.cpp
@@ -0,0 +1,52 @@
+// Copyright 2015 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "ArcGISMapImageLayerUrl.h"
+
+#include "Map.h"
+#include "MapQuickView.h"
+#include "Basemap.h"
+#include "ArcGISMapImageLayer.h"
+#include <QUrl>
+
+using namespace Esri::ArcGISRuntime;
+
+ArcGISMapImageLayerUrl::ArcGISMapImageLayerUrl(QQuickItem* parent) :
+    QQuickItem(parent)
+{
+}
+
+// destructor
+ArcGISMapImageLayerUrl::~ArcGISMapImageLayerUrl()
+{
+}
+
+void ArcGISMapImageLayerUrl::componentComplete()
+{
+    QQuickItem::componentComplete();
+
+    // find QML MapView component
+    m_mapView = findChild<MapQuickView*>("mapView");
+
+    // create a new map image layer
+    ArcGISMapImageLayer* mapImageLayer = new ArcGISMapImageLayer(QUrl("http://sampleserver5.arcgisonline.com/arcgis/rest/services/Elevation/WorldElevations/MapServer"), this);
+    // create a new basemap instance
+    Basemap* basemap = new Basemap(this);
+    // add the map image layer to the basemap
+    basemap->baseLayers()->append(mapImageLayer);
+    // create a new map instance
+    m_map = new Map(basemap, this);
+    // set map on the map view
+    m_mapView->setMap(m_map);
+}
+
