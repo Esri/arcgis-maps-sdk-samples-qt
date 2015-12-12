e143b52dbbee202551c0ccc7ce594cbe530a8391
Michael Tims <mtims@esri.com>
2015-12-11 16:25:57 -0800

    Fix legal comment build error

--- ArcGISRuntimeSDKQt_CppSamples/Maps/SetInitialMapLocation/SetInitialMapLocation.h
+++ ArcGISRuntimeSDKQt_CppSamples/Maps/SetInitialMapLocation/SetInitialMapLocation.h
@@ -12,7 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-s// [Legal]
+// [Legal]
 
 #ifndef SET_INITIAL_MAP_LOCATION_H
 #define SET_INITIAL_MAP_LOCATION_H
