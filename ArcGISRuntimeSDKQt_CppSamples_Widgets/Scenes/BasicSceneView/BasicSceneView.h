900326fd7fe9a6d7f8e54599c35dd7e38e5a24b3
kous3106 <khajra@esri.com>
2016-06-21 17:23:31 -0700

    Widgets sample for basic sceneview

---
+++ ArcGISRuntimeSDKQt_CppSamples_Widgets/Scenes/BasicSceneView/BasicSceneView.h
@@ -0,0 +1,42 @@
+// [WriteFile Name=DisplayMap, Category=Maps]
+// [Legal]
+// Copyright 2015 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#ifndef BASIC_SCENEVIEW_H
+#define BASIC_SCENEVIEW_H
+
+namespace Esri {
+namespace ArcGISRuntime {
+  class Scene;
+  class SceneGraphicsView;
+  }
+}
+
+#include <QWidget>
+
+class BasicSceneView : public QWidget
+{
+  Q_OBJECT
+
+public:
+  explicit BasicSceneView(QWidget* parent = 0);
+   ~BasicSceneView();
+
+private:
+  Esri::ArcGISRuntime::Scene* m_scene;
+  Esri::ArcGISRuntime::SceneGraphicsView* m_sceneView;
+};
+
+#endif // BASIC_SCENEVIEW_H
