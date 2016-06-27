7e105a6bc507df22f1a15688dec721a04f3e1515
Koushik Hajra <khajra@users.noreply.github.com>
2016-06-27 09:20:37 -0700

    Kous3106/extrusion sample (#187)
    * Update the category name.
    
    * add the extrude sample.
    
    * add fix for gt qt56
    
    * update category
    
    * Update category
    
    * qml sample
    
    * Updated png.
    
    * Fixed folder.
    
    * fix path in the cpp sample.
    
    * use a new graphics everytime.
    
    * Fix the scene layer sample.
    
    * deleted readme's
    
    * fix tabs
    
    * more tax fixes and screenshot fixes.
    
    * fixed tabs
    
    * Fix spacing

---
+++ ArcGISRuntimeSDKQt_CppSamples/Scenes/BasicSceneView/BasicSceneView.cpp
@@ -0,0 +1,61 @@
+// [WriteFile Name=BasicSceneView, Category=Scenes]
+// [Legal]
+// Copyright 2016 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#include "BasicSceneView.h"
+
+#include "Scene.h"
+#include "SceneQuickView.h"
+#include "Basemap.h"
+#include "ArcGISTiledElevationSource.h"
+
+using namespace Esri::ArcGISRuntime;
+
+BasicSceneView::BasicSceneView(QQuickItem* parent) :
+    QQuickItem(parent),
+    m_scene(nullptr),
+    m_sceneView(nullptr)
+{
+}
+
+BasicSceneView::~BasicSceneView()
+{
+}
+
+void BasicSceneView::componentComplete()
+{
+    QQuickItem::componentComplete();
+
+    // find QML SceneView component
+    m_sceneView = findChild<SceneQuickView*>("sceneView");
+
+    // create a new basemap instance
+    Basemap* basemap = Basemap::imagery(this);
+    // create a new scene instance
+    m_scene = new Scene(basemap, this);
+    // set scene on the scene view
+    m_sceneView->setScene(m_scene);
+
+    // create a new elevation source
+    ArcGISTiledElevationSource* elevationSource = new ArcGISTiledElevationSource(QUrl("http://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"), this);
+    // add the elevation source to the scene to display elevation
+    m_scene->baseSurface()->elevationSources()->append(elevationSource);
+
+    // create a camera
+    Camera camera(28.4, 83.9, 10010.0, 10.0, 80.0, 300.0);
+    // set the viewpoint
+    m_sceneView->setViewpointCamera(camera);
+}
+
---
+++ ArcGISRuntimeSDKQt_QMLSamples/Scenes/ExtrudeGraphics/README.md
@@ -0,0 +1,22 @@
+#Basic SceneView (URL)
+
+This sample demonstrates how to render graphics extruded in the Z direction.
+
+![](screenshot.png)
+
+##How it works
+To extrude graphics according to a property:
+
+1. Create a ```GraphicsOverlay``` and ```SimpleRenderer```.
+2. Get the renderer's ```SceneProperties``` using ```renderer->sceneProperties()```.
+3. Set the extrusion mode for the renderer with ```props.setExtrusionMode(ExtrusionMode::BaseHeight)```.
+4. Specify the attribute name of the graphic that the extrusion mode will use: ```props.setExtrusionExpression("height")```.
+5. Set the renderer on the graphics overlay using ```graphicsOverlay->setRenderer(renderer)```.
+6. Create graphics with the attribute set: ```graphic->attributes()->insertAttribute("height", z)```.
+
+##Features
+- ArcGISScene
+- Graphic
+- GraphicsOverlay
+- Renderer
+- Renderer->SceneProperties
