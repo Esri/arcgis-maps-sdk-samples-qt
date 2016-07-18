e29007f1ed9b158b542ff7588e5f2ffbc68d5198
Ryan Kim <ryan8759@esri.com>
2016-07-18 14:58:54 -0700

    qrc path fixes

--- ArcGISRuntimeSDKQt_QMLSamples/DisplayInformation/Picture_Marker_Symbol/main.cpp
+++ ArcGISRuntimeSDKQt_QMLSamples/DisplayInformation/Picture_Marker_Symbol/main.cpp
@@ -47,7 +47,7 @@ int main(int argc, char *argv[])
     view.engine()->addImportPath(QDir(QCoreApplication::applicationDirPath()).filePath("qml"));
 
     // Set the source
-    view.setSource(QUrl("qrc:/Samples/DisplayInformation/Picture_Marker_Symbol/Picture_Marker_Symbol.qml"));
+    view.setSource(QUrl("qrc:/Samples/Display Information/Picture_Marker_Symbol/Picture_Marker_Symbol.qml"));
 
     view.show();
 
