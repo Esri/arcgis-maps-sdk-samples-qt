a1ef8c41964cfcaf07efaadd7e0d22e88a6ed20f
Ryan Kim <ryan8759@esri.com>
2016-08-15 09:55:05 -0700

    created c++ sample

---
+++ ArcGISRuntimeSDKQt_CppSamples/Search/OfflineGeocode/OfflineGeocode.cpp
@@ -0,0 +1,50 @@
+// [WriteFile Name=OfflineGeocode, Category=Search]
+// [Legal]
+// Copyright 2016 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#include "OfflineGeocode.h"
+
+#include "Map.h"
+#include "MapQuickView.h"
+
+using namespace Esri::ArcGISRuntime;
+
+OfflineGeocode::OfflineGeocode(QQuickItem* parent /* = nullptr */):
+    QQuickItem(parent),
+    m_map(nullptr),
+    m_mapView(nullptr)
+{
+}
+
+OfflineGeocode::~OfflineGeocode()
+{
+}
+
+void OfflineGeocode::componentComplete()
+{
+    QQuickItem::componentComplete();
+
+    // find QML MapView component
+    m_mapView = findChild<MapQuickView*>("mapView");
+    m_mapView->setWrapAroundMode(WrapAroundMode::Disabled);
+
+    // Create a map using the topographic basemap
+    m_map = new Map(Basemap::topographic(this), this);
+    m_map->setInitialViewpoint(Viewpoint(Envelope(-13075816.4047166, 4014771.46954516, -13073005.6797177, 4016869.78617381, SpatialReference(102100))));
+
+
+    // Set map to map view
+    m_mapView->setMap(m_map);
+}
