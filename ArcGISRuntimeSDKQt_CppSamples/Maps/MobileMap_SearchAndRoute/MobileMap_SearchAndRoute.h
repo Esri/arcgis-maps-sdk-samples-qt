a3b5de3dfc222a69cd40567c62864f71b5c80e4c
Ryan Kim <ryan8759@esri.com>
2016-08-23 13:35:11 -0700

    created c++ project

---
+++ ArcGISRuntimeSDKQt_CppSamples/Maps/MobileMap_SearchAndRoute/MobileMap_SearchAndRoute.h
@@ -0,0 +1,46 @@
+// [WriteFile Name=MobileMap_SearchAndRoute, Category=Maps]
+// [Legal]
+// Copyright 2016 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#ifndef MOBILEMAP_SEARCHANDROUTE_H
+#define MOBILEMAP_SEARCHANDROUTE_H
+
+namespace Esri
+{
+    namespace ArcGISRuntime
+    {
+        class Map;
+        class MapQuickView;
+    }
+}
+
+#include <QQuickItem>
+
+class MobileMap_SearchAndRoute : public QQuickItem
+{
+    Q_OBJECT
+
+public:
+    MobileMap_SearchAndRoute(QQuickItem* parent = nullptr);
+    ~MobileMap_SearchAndRoute();
+
+    void componentComplete() Q_DECL_OVERRIDE;
+
+private:
+    Esri::ArcGISRuntime::Map* m_map;
+    Esri::ArcGISRuntime::MapQuickView* m_mapView;
+};
+
+#endif // MOBILEMAP_SEARCHANDROUTE_H
