8cf335256760c467cdb89ec7c5b4373fd6f12d50
Michael Tims <mtims@esri.com>
2016-09-10 09:09:37 -0700

    Fix mouseClicked build error

--- ArcGISRuntimeSDKQt_CppSamples/EditData/EditAndSyncFeatures/EditAndSyncFeatures.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/EditData/EditAndSyncFeatures/EditAndSyncFeatures.cpp
@@ -92,7 +92,7 @@ void EditAndSyncFeatures::componentComplete()
 void EditAndSyncFeatures::connectSignals()
 {
     // lambda expression for the mouse press event on the mapview
-    connect(m_mapView, &MapQuickView::mouseClick, [this](QMouseEvent& mouseEvent)
+    connect(m_mapView, &MapQuickView::mouseClicked, [this](QMouseEvent& mouseEvent)
     {
         if (m_isOffline)
         {
