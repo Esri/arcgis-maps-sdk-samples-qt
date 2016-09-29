8d1013b74b7da2a18b75b2701fa334a9b718e8fc
Michael Tims <mtims@esri.com>
2016-09-29 13:30:44 -0700

    Remove Qt 5.7 ANGLE workaround

--- ArcGISRuntimeSDKQt_CppSamples/DisplayInformation/ShowCallout/main.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/DisplayInformation/ShowCallout/main.cpp
@@ -16,7 +16,6 @@
 #include <QCommandLineParser>
 #include <QDir>
 #include <QQmlEngine>
-#include <QSurfaceFormat>
 
 #ifdef Q_OS_WIN
 #include <Windows.h>
@@ -31,17 +30,6 @@ using namespace Esri::ArcGISRuntime;
 
 int main(int argc, char *argv[])
 {
-#if defined(Q_OS_WIN)
-  if (QT_VERSION_MAJOR == 5 && QT_VERSION_MINOR > 6)
-  {
-    // Workaround for Qt versions greater than 5.6
-    // Force to OpenGL ES 3
-    QSurfaceFormat fmt = QSurfaceFormat::defaultFormat();
-    fmt.setVersion(3, 0);
-    QSurfaceFormat::setDefaultFormat(fmt);
-  }
-#endif
-
   QGuiApplication app(argc, argv);
 
 #ifdef Q_OS_WIN
