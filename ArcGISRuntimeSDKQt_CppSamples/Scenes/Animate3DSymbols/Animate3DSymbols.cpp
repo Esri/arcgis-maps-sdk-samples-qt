52fb1d0b15c70343ddd2dfc48e20009ef1863069
Michael Tims <mtims@esri.com>
2016-10-31 17:02:32 -0700

    Cleanup MissionData and move class into Animate3DSymbols files (#336)

--- ArcGISRuntimeSDKQt_CppSamples/Scenes/Animate3DSymbols/Animate3DSymbols.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/Scenes/Animate3DSymbols/Animate3DSymbols.cpp
@@ -15,7 +15,6 @@
 // [Legal]
 
 #include "Animate3DSymbols.h"
-#include "MissionData.h"
 
 #include "ArcGISTiledElevationSource.h"
 #include "Camera.h"
@@ -415,4 +414,76 @@ double Animate3DSymbols::angle() const
 int Animate3DSymbols::speed() const
 {
   return m_camHandler ? m_camHandler->m_intervalMs : 50;
+}
+
+// MissionData
+
+MissionData::MissionData():
+  m_ready(false)
+{
+}
+
+MissionData::~MissionData()
+{
+
+}
+
+bool MissionData::parse(const QString &dataPath)
+{
+  m_data.clear();
+  m_ready = false;
+
+  QFile file(dataPath);
+  if(!file.exists())
+    return false;
+
+  if (!file.open(QIODevice::ReadOnly))
+    return false;
+
+  while (!file.atEnd())
+  {
+    QByteArray line = file.readLine();
+    QList<QByteArray> parts = line.split(',');
+    if(parts.size() < 6)
+      continue;
+
+    bool ok = false;
+    double lon = parts.at(0).toDouble(&ok);
+    if(!ok)
+      continue;
+
+    double lat = parts.at(1).toDouble(&ok);
+    if(!ok)
+      continue;
+
+    double elevation = parts.at(2).toDouble(&ok);
+    if(!ok)
+      continue;
+
+    double heading = parts.at(3).toDouble(&ok);
+    if(!ok)
+      continue;
+
+    double pitch = parts.at(4).toDouble(&ok);
+    if(!ok)
+      continue;
+
+    double roll = parts.at(5).simplified().toDouble(&ok);
+    if(!ok)
+      continue;
+
+    m_data.emplace_back((double)lon, (double)lat, (double)elevation, (double)heading, (double)pitch, (double)roll);
+  }
+
+  m_ready = m_data.size() > 0;
+  return m_ready;
+}
+
+const MissionData::DataPoint &MissionData::dataAt(size_t i) const
+{
+  if(i < m_data.size())
+    return m_data[i];
+
+  static MissionData::DataPoint dataPoint;
+  return dataPoint;
 }
--- ArcGISRuntimeSDKQt_CppSamples/Scenes/Animate3DSymbols/Animate3DSymbols.h
+++ ArcGISRuntimeSDKQt_CppSamples/Scenes/Animate3DSymbols/Animate3DSymbols.h
@@ -34,9 +34,12 @@ namespace Esri
 
 class QAbstractItemModel;
 
+#include "Point.h"
+
 #include <QQuickItem>
 #include <QString>
 
+#include <math.h>
 #include <memory>
 
 class Animate3DSymbols : public QQuickItem
@@ -112,6 +115,41 @@ private:
   int m_frame;
   bool m_following;
   double m_mapZoomFactor;
+};
+
+class MissionData
+{
+public:
+
+  struct DataPoint
+  {
+    DataPoint(){}
+    DataPoint(double lon, double lat, double elevation, double heading, double pitch, double roll):
+      m_pos(Esri::ArcGISRuntime::Point(lon, lat, elevation, Esri::ArcGISRuntime::SpatialReference::wgs84())),
+      m_heading(heading),
+      m_pitch(pitch),
+      m_roll(roll){}
+
+    Esri::ArcGISRuntime::Point m_pos;
+    double m_heading = NAN;
+    double m_pitch = NAN;
+    double m_roll = NAN;
+  };
+
+  typedef std::vector<DataPoint> DataPointList;
+
+  MissionData();
+  ~MissionData();
+
+  bool parse(const QString& dataPath);
+  bool isEmpty() const {return m_data.empty();}
+  size_t size() const {return m_data.size();}
+  const DataPoint& dataAt(size_t i) const;
+  bool ready() const {return m_ready;}
+
+private:
+  DataPointList m_data;
+  bool m_ready;
 };
 
 #endif // ANIMATE3DSYMBOLS_H
