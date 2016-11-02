0bb9147d42cce4cbe7775537cb96f976389b499e
Luke Smallwood <luke8660@esri.com>
2016-11-02 11:54:25 +0000

    Initial commit for Cpp Basemap List sample

---
+++ ArcGISRuntimeSDKQt_CppSamples/CloudAndPortal/ShowOrgBasemapPicker/ShowOrgBasemapPicker.cpp
@@ -0,0 +1,150 @@
+// Copyright 2016 ESRI
+//
+// All rights reserved under the copyright laws of the United States
+// and applicable international laws, treaties, and conventions.
+//
+// You may freely redistribute and use this sample code, with or
+// without modification, provided you include the original copyright
+// notice and use restrictions.
+//
+// See the Sample code usage restrictions document for further information.
+//
+
+#include "AuthenticationManager.h"
+#include "Map.h"
+#include "MapQuickView.h"
+#include "Basemap.h"
+#include "BasemapListModel.h"
+#include "Portal.h"
+#include "PortalItem.h"
+#include "PortalItemListModel.h"
+#include "PortalQueryParametersForItems.h"
+
+#include "ShowOrgBasemapPicker.h"
+
+using namespace Esri::ArcGISRuntime;
+
+ShowOrgBasemapPicker::ShowOrgBasemapPicker(QQuickItem* parent /* = nullptr */):
+    QQuickItem(parent),
+    m_map(nullptr),
+    m_mapView(nullptr),
+    m_portal(new Portal(new Credential(OAuthClientInfo("W3hPKzPbeJ0tr8aj", OAuthMode::User), this), this))
+{
+    connect(m_portal, &Portal::basemapsChanged, this, &ShowOrgBasemapPicker::onFetchBasemapsCompleted);
+    emit authManagerChanged();
+    AuthenticationManager::instance()->setCredentialCacheEnabled(false);
+}
+
+ShowOrgBasemapPicker::~ShowOrgBasemapPicker()
+{
+}
+
+void ShowOrgBasemapPicker::componentComplete()
+{
+    QQuickItem::componentComplete();
+
+    connect(m_portal, &Portal::loadStatusChanged, this, [this]()
+        {
+            m_portalLoaded = m_portal->loadStatus() == LoadStatus::Loaded;
+            emit portalLoadedChanged();
+            emit orgNameChanged();
+
+            if (m_portalLoaded)
+                m_portal->fetchBasemaps();
+        }
+    );
+    m_portal->load();
+
+    // find QML MapView component
+    m_mapView = findChild<MapQuickView*>("mapView");
+    m_mapView->setWrapAroundMode(WrapAroundMode::Disabled);
+}
+
+bool ShowOrgBasemapPicker::portalLoaded() const
+{
+    return m_portalLoaded;
+}
+
+QString ShowOrgBasemapPicker::orgName() const
+{
+    if (!portalLoaded() || !m_portal->portalInfo())
+        return "";
+
+    return m_portal->portalInfo()->organizationName();
+}
+
+QAbstractListModel* ShowOrgBasemapPicker::basemaps() const
+{
+    return m_portal->basemaps();
+}
+
+QString ShowOrgBasemapPicker::mapLoadError() const
+{
+    return m_mapLoadeError;
+}
+
+void ShowOrgBasemapPicker::loadSelectedBasemap(int index)
+{
+    if (!m_portal->basemaps())
+        return;
+
+    Basemap* selectedBasemap = m_portal->basemaps()->at(index);
+    if (!selectedBasemap)
+        return;
+
+    if (m_map)
+        m_map->disconnect();
+
+    m_mapLoadeError.clear();
+    emit mapLoadErrorChanged();
+
+    m_map = new Map(selectedBasemap, this);
+
+    connect(m_map, &Map::errorOccurred, this, [this]()
+        {
+            m_mapLoadeError = m_map->loadError().message();
+            emit mapLoadErrorChanged();
+        }
+    );
+
+    connect(m_map, &Map::loadStatusChanged, this, [this]()
+        {
+            if (!m_map || m_map->loadStatus() != LoadStatus::Loaded)
+                return;
+
+            m_mapView->setMap(m_map);
+            m_mapView->setVisible(true);
+        }
+    );
+
+    m_map->load();
+}
+
+void ShowOrgBasemapPicker::errorAccepted()
+{
+    m_mapLoadeError.clear();
+    emit mapLoadErrorChanged();
+}
+
+QString ShowOrgBasemapPicker::basemapTitle(int index)
+{
+    if (!basemaps())
+        return "";
+
+    Basemap* basemapAtIndex = m_portal->basemaps()->at(index);
+
+    if (!basemapAtIndex)
+        return "";
+
+    return basemapAtIndex->item()->title();
+}
+
+void ShowOrgBasemapPicker::onFetchBasemapsCompleted()
+{
+    emit basemapsChanged();
+}
+
+AuthenticationManager *ShowOrgBasemapPicker::authManager() const
+{
+    return AuthenticationManager::instance();
+}
---
+++ ArcGISRuntimeSDKQt_CppSamples/CloudAndPortal/ShowOrgBasemapPicker/ShowOrgBasemapPicker.h
@@ -0,0 +1,75 @@
+// Copyright 2016 ESRI
+//
+// All rights reserved under the copyright laws of the United States
+// and applicable international laws, treaties, and conventions.
+//
+// You may freely redistribute and use this sample code, with or
+// without modification, provided you include the original copyright
+// notice and use restrictions.
+//
+// See the Sample code usage restrictions document for further information.
+//
+
+#ifndef SHOWORGBASEMAPPICKER_H
+#define SHOWORGBASEMAPPICKER_H
+
+namespace Esri
+{
+    namespace ArcGISRuntime
+    {
+        class AuthenticationManager;
+        class BasemapListModel;
+        class Map;
+        class MapQuickView;
+        class Portal;
+    }
+}
+
+#include <QAbstractListModel>
+#include <QQuickItem>
+
+class ShowOrgBasemapPicker : public QQuickItem
+{
+    Q_OBJECT
+
+    Q_PROPERTY(Esri::ArcGISRuntime::AuthenticationManager* authManager READ authManager NOTIFY authManagerChanged)
+    Q_PROPERTY(bool portalLoaded READ portalLoaded NOTIFY portalLoadedChanged)
+    Q_PROPERTY(QString orgName READ orgName NOTIFY orgNameChanged)
+    Q_PROPERTY(QAbstractListModel* basemaps READ basemaps NOTIFY basemapsChanged)
+    Q_PROPERTY(QString mapLoadError READ mapLoadError NOTIFY mapLoadErrorChanged)
+
+public:
+    ShowOrgBasemapPicker(QQuickItem* parent = nullptr);
+    ~ShowOrgBasemapPicker();
+
+    void componentComplete() Q_DECL_OVERRIDE;
+
+    Esri::ArcGISRuntime::AuthenticationManager* authManager() const;
+    bool portalLoaded() const;
+    QString orgName() const;
+    QAbstractListModel* basemaps() const;
+    QString mapLoadError() const;
+
+    Q_INVOKABLE void loadSelectedBasemap(int index);
+    Q_INVOKABLE void errorAccepted();
+    Q_INVOKABLE QString basemapTitle(int index);
+
+signals:
+    void authManagerChanged();
+    void portalLoadedChanged();
+    void orgNameChanged();
+    void basemapsChanged();
+    void mapLoadErrorChanged();
+
+private slots:
+    void onFetchBasemapsCompleted();
+
+private:
+    Esri::ArcGISRuntime::Map* m_map;
+    Esri::ArcGISRuntime::MapQuickView* m_mapView;
+    Esri::ArcGISRuntime::Portal* m_portal;
+    bool m_portalLoaded;
+    QString m_mapLoadeError;
+};
+
+#endif // SHOWORGBASEMAPPICKER_H
