4f5cbe901e573f037520d22efb9f7bbd4728413b
James Ballard <jballard@esri.com>
2016-11-09 09:23:37 -0800

    Use static rendering mode for 3D graphics overlays.

--- ArcGISRuntimeSDKQt_CppSamples/DisplayInformation/GraphicsOverlayDictionaryRenderer_3D/GraphicsOverlayDictionaryRenderer_3D.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/DisplayInformation/GraphicsOverlayDictionaryRenderer_3D/GraphicsOverlayDictionaryRenderer_3D.cpp
@@ -38,7 +38,7 @@ GraphicsOverlayDictionaryRenderer_3D::GraphicsOverlayDictionaryRenderer_3D(QQuic
     m_sceneView(nullptr),
     m_graphicsOverlay(new GraphicsOverlay(this))
 {
-    m_graphicsOverlay->setRenderingMode(GraphicsRenderingMode::Dynamic);
+    m_graphicsOverlay->setRenderingMode(GraphicsRenderingMode::Static);
 }
 
 GraphicsOverlayDictionaryRenderer_3D::~GraphicsOverlayDictionaryRenderer_3D()
