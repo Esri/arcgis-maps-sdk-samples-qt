06986ef87d35e8c0415184c9a5522ddef8ff43bc
Luke Smallwood <LSmallwood@esri.com>
2017-03-23 12:54:24 +0000

    C++ Sample

---
+++ ArcGISRuntimeSDKQt_CppSamples/Routing/ClosestFacilitySample/ClosestFacilitySample.h
@@ -0,0 +1,79 @@
+// [WriteFile Name=ClosestFacilitySample, Category=Routing]
+// [Legal]
+// Copyright 2017 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#ifndef CLOSESTFACILITYSAMPLE_H
+#define CLOSESTFACILITYSAMPLE_H
+
+namespace Esri
+{
+    namespace ArcGISRuntime
+    {
+        class ClosestFacilityTask;
+        class Facility;
+        class GraphicsOverlay;
+        class Map;
+        class MapQuickView;
+        class Point;
+    }
+}
+
+#include "ClosestFacilityParameters.h"
+
+#include <QList>
+#include <QQuickItem>
+#include <QUrl>
+
+class ClosestFacilitySample : public QQuickItem
+{
+    Q_OBJECT
+
+    Q_PROPERTY(bool busy READ busy NOTIFY busyChanged)
+    Q_PROPERTY(QString message READ message NOTIFY messageChanged)
+
+public:
+    explicit ClosestFacilitySample(QQuickItem* parent = nullptr);
+    ~ClosestFacilitySample();
+
+    void componentComplete() Q_DECL_OVERRIDE;
+
+signals:
+    void busyChanged();
+    void messageChanged();
+
+private:
+    bool busy() const;
+    QString message() const;
+
+    void setBusy(bool val);
+    void createFacilities();
+    void createGraphics();
+    void setupRouting();
+    void solveRoute(const Esri::ArcGISRuntime::Point& incidentPoint);
+
+    Esri::ArcGISRuntime::Map* m_map = nullptr;
+    Esri::ArcGISRuntime::MapQuickView* m_mapView = nullptr;
+    QList<Esri::ArcGISRuntime::Facility> m_facilities;
+    Esri::ArcGISRuntime::ClosestFacilityTask* m_task;
+    Esri::ArcGISRuntime::ClosestFacilityParameters m_facilityParams;
+    Esri::ArcGISRuntime::GraphicsOverlay* m_resultsOverlay;
+    bool m_busy = false;
+    QString m_message;
+
+    static const QUrl facilityImageUrl;
+    static const QUrl sanDiegoRegion;
+};
+
+#endif // CLOSESTFACILITYSAMPLE_H
