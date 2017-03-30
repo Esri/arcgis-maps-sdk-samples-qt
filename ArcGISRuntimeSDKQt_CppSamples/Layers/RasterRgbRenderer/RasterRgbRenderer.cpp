9321d0aed938e06f7a4096c90a6941214428092c
Luke Smallwood <lsmallwood@esri.com>
2017-03-30 15:04:25 +0100

    RgbRenderer sample for C++

---
+++ ArcGISRuntimeSDKQt_CppSamples/Layers/RasterRgbRenderer/RasterRgbRenderer.cpp
@@ -0,0 +1,80 @@
+// [WriteFile Name=RasterRgbRenderer, Category=Layers]
+// [Legal]
+// Copyright 2017 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#include "RasterRgbRenderer.h"
+
+#include "Map.h"
+#include "MapQuickView.h"
+#include "MinMaxStretchParameters.h"
+#include "PercentClipStretchParameters.h"
+#include "Raster.h"
+#include "RasterLayer.h"
+#include "RGBRenderer.h"
+#include "StandardDeviationStretchParameters.h"
+
+#include <QStringListModel>
+#include <QQmlProperty>
+
+using namespace Esri::ArcGISRuntime;
+
+RasterRgbRenderer::RasterRgbRenderer(QQuickItem* parent /* = nullptr */):
+    QQuickItem(parent)
+{
+}
+
+RasterRgbRenderer::~RasterRgbRenderer()
+{
+}
+
+void RasterRgbRenderer::componentComplete()
+{
+    QQuickItem::componentComplete();
+
+    // find QML MapView component
+    m_mapView = findChild<MapQuickView*>("mapView");
+    m_mapView->setWrapAroundMode(WrapAroundMode::Disabled);
+
+    // Create the raster and raster layer
+    m_dataPath = QUrl(QQmlProperty::read(this, "dataPath").toString()).toLocalFile();
+    Raster* raster = new Raster(m_dataPath + "/Shasta.tif", this);
+    m_rasterLayer = new RasterLayer(raster, this);
+
+    // Add the raster to the map
+    Basemap* basemap = new Basemap(m_rasterLayer, this);
+    Map* map = new Map(basemap, this);
+    m_mapView->setMap(map);
+}
+
+void RasterRgbRenderer::applyMinMax(double min0, double min1, double min2, double max0, double max1, double max2)
+{
+    MinMaxStretchParameters stretchParams(QList<double>{min0, min1, min2}, QList<double>{max0, max1, max2});
+    RGBRenderer* renderer = new RGBRenderer(stretchParams, this);
+    m_rasterLayer->setRenderer(renderer);
+}
+
+void RasterRgbRenderer::applyPercentClip(double min, double max)
+{
+    PercentClipStretchParameters stretchParams(min, max);
+    RGBRenderer* renderer = new RGBRenderer(stretchParams, this);
+    m_rasterLayer->setRenderer(renderer);
+}
+
+void RasterRgbRenderer::applyStandardDeviation(double factor)
+{
+    StandardDeviationStretchParameters stretchParams(factor);
+    RGBRenderer* renderer = new RGBRenderer(stretchParams, this);
+    m_rasterLayer->setRenderer(renderer);
+}
---
+++ ArcGISRuntimeSDKQt_CppSamples/Layers/RasterRgbRenderer/RasterRgbRenderer.h
@@ -0,0 +1,53 @@
+// [WriteFile Name=RasterRgbRenderer, Category=Layers]
+// [Legal]
+// Copyright 2017 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#ifndef RGBRENDERER_H
+#define RGBRENDERER_H
+
+namespace Esri
+{
+namespace ArcGISRuntime
+{
+class Map;
+class MapQuickView;
+class RasterLayer;
+}
+}
+
+#include <QQuickItem>
+
+class RasterRgbRenderer : public QQuickItem
+{
+    Q_OBJECT
+
+public:
+    explicit RasterRgbRenderer(QQuickItem* parent = nullptr);
+    ~RasterRgbRenderer();
+
+    void componentComplete() Q_DECL_OVERRIDE;
+
+    Q_INVOKABLE void applyMinMax(double min0, double min1, double min2, double max0, double max1, double max2);
+    Q_INVOKABLE void applyPercentClip(double min, double max);
+    Q_INVOKABLE void applyStandardDeviation(double factor);
+
+private:
+    Esri::ArcGISRuntime::Map* m_map = nullptr;
+    Esri::ArcGISRuntime::MapQuickView* m_mapView = nullptr;
+    Esri::ArcGISRuntime::RasterLayer* m_rasterLayer = nullptr;
+    QString m_dataPath;
+};
+
+#endif // RGBRENDERER_H
