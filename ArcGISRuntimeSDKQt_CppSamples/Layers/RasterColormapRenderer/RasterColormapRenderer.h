47dbd4252dbd519a926b5c6d263edaf72b15d704
Luke Smallwood <lsmallwood@esri.com>
2017-04-07 12:31:04 +0100

    C++ sample

---
+++ ArcGISRuntimeSDKQt_CppSamples/Layers/RasterColormapRenderer/RasterColormapRenderer.h
@@ -0,0 +1,54 @@
+// [WriteFile Name=RasterColormapRenderer, Category=Layers]
+// [Legal]
+// Copyright 2017 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#ifndef COLORMAPRENDERER_H
+#define COLORMAPRENDERER_H
+
+namespace Esri
+{
+  namespace ArcGISRuntime
+  {
+    class ColormapRenderer;
+    class Map;
+    class MapQuickView;
+    class RasterLayer;
+  }
+}
+
+#include <QQuickItem>
+
+class RasterColormapRenderer : public QQuickItem
+{
+  Q_OBJECT
+
+public:
+  explicit RasterColormapRenderer(QQuickItem* parent = nullptr);
+  ~RasterColormapRenderer();
+
+  static void init();
+
+  void componentComplete() Q_DECL_OVERRIDE;
+
+private:
+  Esri::ArcGISRuntime::ColormapRenderer* createRenderer();
+
+  Esri::ArcGISRuntime::Map* m_map = nullptr;
+  Esri::ArcGISRuntime::MapQuickView* m_mapView = nullptr;
+  Esri::ArcGISRuntime::RasterLayer* m_rasterLayer = nullptr;
+  QString m_dataPath;
+};
+
+#endif // COLORMAPRENDERER_H
