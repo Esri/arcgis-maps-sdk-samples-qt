047d732f2fae8a6987340e42c896e28c05f79405
Thomas A. Dunn <tdunn@esri.com>
2017-04-10 07:35:06 -0700

    Added Linux path string replacement logic.

--- ArcGISRuntimeSDKQt_QMLSamples/Geometry/FormatCoordinates/main.cpp
+++ ArcGISRuntimeSDKQt_QMLSamples/Geometry/FormatCoordinates/main.cpp
@@ -23,28 +23,36 @@
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
-    
+  QGuiApplication app(argc, argv);
+
 #ifdef Q_OS_WIN
-    // Force usage of OpenGL ES through ANGLE on Windows
-    QCoreApplication::setAttribute(Qt::AA_UseOpenGLES);
+  // Force usage of OpenGL ES through ANGLE on Windows
+  QCoreApplication::setAttribute(Qt::AA_UseOpenGLES);
 #endif
 
-    // Intialize application view
-    QQuickView view;
-    view.setResizeMode(QQuickView::SizeRootObjectToView);
+  // Intialize application view
+  QQuickView view;
+  view.setResizeMode(QQuickView::SizeRootObjectToView);
 
-    // Add the import Path
-    view.engine()->addImportPath(QDir(QCoreApplication::applicationDirPath()).filePath("qml"));
-    // Add the Runtime and Extras path
-    view.engine()->addImportPath(QUOTE(ARCGIS_RUNTIME_IMPORT_PATH));
-    // Add the Toolkit path
-    view.engine()->addImportPath(QUOTE(ARCGIS_TOOLKIT_IMPORT_PATH));
+  // Add the import Path
+  view.engine()->addImportPath(QDir(QCoreApplication::applicationDirPath()).filePath("qml"));
+  // Add the Runtime and Extras path
+  view.engine()->addImportPath(QUOTE(ARCGIS_RUNTIME_IMPORT_PATH));
+  // Add the Toolkit path
+  view.engine()->addImportPath(QUOTE(ARCGIS_TOOLKIT_IMPORT_PATH));
 
-    // Set the source
-    view.setSource(QUrl("qrc:/Samples/Geometry/FormatCoordinates/FormatCoordinates.qml"));
+#if defined(LINUX_PLATFORM_REPLACEMENT)
+  // on some linux platforms the string 'linux' is replaced with 1
+  // fix the replacement paths which were created
+  QString replaceString = QUOTE(LINUX_PLATFORM_REPLACEMENT);
+  arcGISRuntimeImportPath = arcGISRuntimeImportPath.replace(replaceString, "linux", Qt::CaseSensitive);
+  arcGISToolkitImportPath = arcGISToolkitImportPath.replace(replaceString, "linux", Qt::CaseSensitive);
+#endif
 
-    view.show();
+  // Set the source
+  view.setSource(QUrl("qrc:/Samples/Geometry/FormatCoordinates/FormatCoordinates.qml"));
 
-    return app.exec();
+  view.show();
+
+  return app.exec();
 }
