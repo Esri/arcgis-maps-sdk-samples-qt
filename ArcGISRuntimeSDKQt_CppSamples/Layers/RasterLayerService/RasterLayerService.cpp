c890daad6f485b7f8c72cb5461beb0e89458e376
kous3106 <khajra@esri.com>
2017-04-24 15:58:49 -0700

    isr sample.

---
+++ ArcGISRuntimeSDKQt_CppSamples/Layers/RasterLayerService/RasterLayerService.cpp
@@ -0,0 +1,69 @@
+// [WriteFile Name=RasterLayerService, Category=Layers]
+// [Legal]
+// Copyright 2017 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#include "RasterLayerService.h"
+
+#include "Map.h"
+#include "MapQuickView.h"
+#include "RasterLayer.h"
+#include "ImageServiceRaster.h"
+
+#include <QQmlProperty>
+
+using namespace Esri::ArcGISRuntime;
+
+RasterLayerService::RasterLayerService(QQuickItem* parent /* = nullptr */):
+  QQuickItem(parent)
+{
+}
+
+RasterLayerService::~RasterLayerService()
+{
+}
+
+void RasterLayerService::init()
+{
+  // Register the map view for QML
+  qmlRegisterType<MapQuickView>("Esri.Samples", 1, 0, "MapView");
+  qmlRegisterType<RasterLayerService>("Esri.Samples", 1, 0, "RasterLayerServiceSample");
+}
+
+void RasterLayerService::componentComplete()
+{
+  QQuickItem::componentComplete();
+
+  // find QML MapView component
+  m_mapView = findChild<MapQuickView*>("mapView");
+  m_mapView->setWrapAroundMode(WrapAroundMode::Disabled);
+
+  // Add a basemap to the map
+  m_map = new Map(Basemap::darkGrayCanvasVector(this), this);
+  m_mapView->setMap(m_map);
+
+  // create an image service raster
+  ImageServiceRaster* imageServiceRaster = new ImageServiceRaster(QUrl("http://sampleserver6.arcgisonline.com/arcgis/rest/services/NLCDLandCover2001/ImageServer"), this);
+  // zoom to the extent of the raster once it is loaded.
+  connect(imageServiceRaster, &ImageServiceRaster::doneLoading, this, [this, imageServiceRaster]()
+  {
+    Viewpoint vp(imageServiceRaster->serviceInfo().fullExtent());
+    m_mapView->setViewpointAnimated(vp, 0.5f, AnimationCurve::EaseInOutQuad);
+  });
+
+  // create a raster layer using the image service raster
+  m_rasterLayer = new RasterLayer(imageServiceRaster, this);
+  // add the raster layer to the map's operational layers
+  m_map->operationalLayers()->append(m_rasterLayer);
+}
---
+++ ArcGISRuntimeSDKQt_CppSamples/Layers/RasterLayerService/RasterLayerService.h
@@ -0,0 +1,53 @@
+// [WriteFile Name=RasterLayerService, Category=Layers]
+// [Legal]
+// Copyright 2017 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#ifndef RASTERLAYERSERVICE_H
+#define RASTERLAYERSERVICE_H
+
+namespace Esri
+{
+  namespace ArcGISRuntime
+  {
+    class Basemap;
+    class Map;
+    class MapQuickView;
+    class RasterLayer;
+  }
+}
+
+#include <QQuickItem>
+
+class RasterLayerService : public QQuickItem
+{
+  Q_OBJECT
+
+public:
+  explicit RasterLayerService(QQuickItem* parent = nullptr);
+  ~RasterLayerService();
+
+  static void init();
+
+  void componentComplete() Q_DECL_OVERRIDE;
+
+private:
+
+  Esri::ArcGISRuntime::Map* m_map = nullptr;
+  Esri::ArcGISRuntime::Basemap* m_basemap = nullptr;
+  Esri::ArcGISRuntime::MapQuickView* m_mapView = nullptr;
+  Esri::ArcGISRuntime::RasterLayer* m_rasterLayer = nullptr;
+};
+
+#endif // RASTERLAYERSERVICE_H
