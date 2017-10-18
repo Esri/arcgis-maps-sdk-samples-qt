310852b9bd63a42323a426a2da1d12e411d65e61
Lucas Danzinger <ldanzinger@esri.com>
2017-10-18 16:18:39 -0500

    Ldanzinger/gpkg raster (#590)
    * initial commit of gpkg raster sample
    
    * adding raster layer geopackage c++ sample
    
    * adding offline data blurb to readme

---
+++ ArcGISRuntimeSDKQt_CppSamples/Layers/RasterLayerGeoPackage/RasterLayerGeoPackage.cpp
@@ -0,0 +1,90 @@
+// [WriteFile Name=RasterLayerGeoPackage, Category=Layers]
+// [Legal]
+// Copyright 2017 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#include "RasterLayerGeoPackage.h"
+
+#include "Map.h"
+#include "MapQuickView.h"
+#include "GeoPackage.h"
+#include "GeoPackageRaster.h"
+#include "RasterLayer.h"
+#include "Point.h"
+#include "Viewpoint.h"
+
+#include <QQmlProperty>
+#include <QUrl>
+
+using namespace Esri::ArcGISRuntime;
+
+RasterLayerGeoPackage::RasterLayerGeoPackage(QQuickItem* parent /* = nullptr */):
+  QQuickItem(parent)
+{
+}
+
+void RasterLayerGeoPackage::init()
+{
+  // Register the map view for QML
+  qmlRegisterType<MapQuickView>("Esri.Samples", 1, 0, "MapView");
+  qmlRegisterType<RasterLayerGeoPackage>("Esri.Samples", 1, 0, "RasterLayerGeoPackageSample");
+}
+
+void RasterLayerGeoPackage::componentComplete()
+{
+  QQuickItem::componentComplete();
+
+  // find QML MapView component
+  m_mapView = findChild<MapQuickView*>("mapView");
+
+  // Create a map using the light gray basemap
+  m_map = new Map(Basemap::lightGrayCanvas(this), this);
+
+  // Set an initial viewpoint
+  const Point pt(-104.8319, 39.7294, SpatialReference(4326));
+  const Viewpoint vp(pt, 200000);
+  m_map->setInitialViewpoint(vp);
+
+  // Create the GeoPackage
+  const QString dataPath = QQmlProperty::read(m_mapView, "dataPath").toUrl().toLocalFile();
+  GeoPackage* gpkg = new GeoPackage(dataPath + "AuroraCO.gpkg", this);
+
+  // Connect to GeoPackage::doneLoading
+  connect(gpkg, &GeoPackage::doneLoading, this, [this, gpkg](const Error e)
+  {
+    if (!e.isEmpty())
+      return;
+
+    if (!(gpkg->geoPackageRasters().size() > 0))
+      return;
+
+    GeoPackageRaster* gpkgRaster = gpkg->geoPackageRasters().at(0);
+    RasterLayer* rasterLayer = new RasterLayer(gpkgRaster, this);
+    m_map->operationalLayers()->append(rasterLayer);
+  });
+
+  // Connect to Map::doneLoading
+  connect(m_map, &Map::doneLoading, this, [this, gpkg](const Error e)
+  {
+    if (!e.isEmpty())
+      return;
+
+    // load the GeoPackage
+    gpkg->load();
+  });
+
+
+  // Set map to map view
+  m_mapView->setMap(m_map);
+}
