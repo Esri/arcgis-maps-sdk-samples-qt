2edc7ac8823e1ce7f3aceaa127fcae299a69e428
Lucas Danzinger <LDanzinger@esri.com>
2018-04-17 16:32:05 -0500

    initial commit

---
+++ ArcGISRuntimeSDKQt_CppSamples/Geometry/SpatialOperations/SpatialOperations.h
@@ -0,0 +1,61 @@
+// [WriteFile Name=SpatialOperations, Category=Geometry]
+// [Legal]
+// Copyright 2018 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#ifndef SpatialOperations_H
+#define SpatialOperations_H
+
+namespace Esri
+{
+  namespace ArcGISRuntime
+  {
+    class Map;
+    class MapQuickView;
+    class GraphicsOverlay;
+  }
+}
+
+#include <QQuickItem>
+#include <QStringList>
+#include "Geometry.h"
+class SpatialOperations : public QQuickItem
+{
+  Q_OBJECT
+  Q_PROPERTY(QStringList geometryOperations READ geometryOperations CONSTANT)
+
+public:
+  explicit SpatialOperations(QQuickItem* parent = nullptr);
+  ~SpatialOperations() = default;
+
+  static void init();
+  void componentComplete() Q_DECL_OVERRIDE;  
+  Q_INVOKABLE void applyGeometryOperation(int index);
+
+private:
+  void addPolygons();
+  void addOutput(const Esri::ArcGISRuntime::Geometry& geometry);
+  QStringList geometryOperations() const { return m_geometryOperations; }
+
+private:
+  Esri::ArcGISRuntime::Map* m_map = nullptr;
+  Esri::ArcGISRuntime::MapQuickView* m_mapView = nullptr;
+  Esri::ArcGISRuntime::Geometry m_polygon1;
+  Esri::ArcGISRuntime::Geometry m_polygon2;
+  Esri::ArcGISRuntime::GraphicsOverlay* m_inputsOverlay = nullptr;
+  Esri::ArcGISRuntime::GraphicsOverlay* m_outputsOverlay = nullptr;
+  QStringList m_geometryOperations;
+};
+
+#endif // SpatialOperations_H
---
+++ ArcGISRuntimeSDKQt_QMLSamples/Geometry/SpatialOperations/README.md
@@ -0,0 +1,10 @@
+# Spatial operations
+
+Demonstrates how to use the GeometryEngine to perform geometry operations between overlapping polygons in a GraphicsOverlay.
+
+![](screenshot.png)
+
+## How it works
+
+## Features
+
