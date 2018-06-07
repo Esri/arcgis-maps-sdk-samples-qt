4743b485264f1163281ba13dbae8a50aa02a949c
Andrew Morris <amorris@esri.com>
2018-06-07 13:43:26 -0700

    updated the Feature (C++) samples to include application name
    Former-commit-id: a5ab3edce9c906e5500ed79105608f60c4423b71

--- ArcGISRuntimeSDKQt_CppSamples/Features/FeatureLayerGeoPackage/main.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/Features/FeatureLayerGeoPackage/main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 {
   QGuiApplication app(argc, argv);
 
-  // Initialize the sample
+app.setApplicationName("FeatureLayerGeoPackage- C++"); 
   FeatureLayer_GeoPackage::init();
 
   // Initialize application view
--- ArcGISRuntimeSDKQt_CppSamples/Features/FeatureLayerGeodatabase/main.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/Features/FeatureLayerGeodatabase/main.cpp
@@ -30,7 +30,7 @@
 int main(int argc, char *argv[])
 {
   QGuiApplication app(argc, argv);
-
+app.setApplicationName("FeatureLayerGeodatabase- C++"); 
   // Initialize the sample
   FeatureLayerGeodatabase::init();
 
