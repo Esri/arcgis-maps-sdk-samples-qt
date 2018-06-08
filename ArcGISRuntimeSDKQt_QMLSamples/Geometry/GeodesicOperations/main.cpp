e77ac33f942d7ca7ab90f9a5fb81df086fe390f4
Andrew Morris <amorris@esri.com>
2018-06-08 10:57:51 -0700

    updated the Geometry (QML) samples to include application name
    Former-commit-id: f4877a77b47ba451418517bbd29ebab16cc5db5f

--- ArcGISRuntimeSDKQt_QMLSamples/Geometry/GeodesicOperations/main.cpp
+++ ArcGISRuntimeSDKQt_QMLSamples/Geometry/GeodesicOperations/main.cpp
@@ -25,6 +25,7 @@ int main(int argc, char *argv[])
 {
 
   QGuiApplication app(argc, argv);
+  app.setApplicationName("GeodesicOperations - C++");
 
   // Intialize application view
   QQuickView view;
