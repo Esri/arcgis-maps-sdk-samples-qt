ea67b39875a6386a303ab22b5a1d24a0a4928f8d
Andrew Morris <amorris@esri.com>
2018-06-08 09:59:28 -0700

    updated the Routing (C++ samples) to include application names
    Former-commit-id: 9700dcb9b9f7bd18992d241cb0a63a7273947aa7

--- ArcGISRuntimeSDKQt_CppSamples/Routing/ServiceArea/main.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/Routing/ServiceArea/main.cpp
@@ -29,8 +29,9 @@
 int main(int argc, char *argv[])
 {
   QGuiApplication app(argc, argv);
+  app.setApplicationName("ServiceArea - C++");
 
-  // Initialize the sample
+// Initialize the sample  // Initialize the sample
   ServiceArea::init();
 
   // Initialize application view
