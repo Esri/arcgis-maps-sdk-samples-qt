f4877a77b47ba451418517bbd29ebab16cc5db5f
Andrew Morris <amorris@esri.com>
2018-06-08 10:57:51 -0700

    updated the Geometry (QML) samples to include application name

--- ArcGISRuntimeSDKQt_QMLSamples/Geometry/CreateGeometries/main.cpp
+++ ArcGISRuntimeSDKQt_QMLSamples/Geometry/CreateGeometries/main.cpp
@@ -25,6 +25,7 @@ int main(int argc, char *argv[])
 {
 
   QGuiApplication app(argc, argv);
+  app.setApplicationName("CreateGeometries - C++");
 
   // Intialize application view
   QQuickView view;
