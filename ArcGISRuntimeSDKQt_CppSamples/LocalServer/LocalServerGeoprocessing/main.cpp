d0d0455396954b208a8778874a8bd71e39c2d7da
Andrew Morris <amorris@esri.com>
2018-06-08 08:29:30 -0700

    updated LocalServer (C++) samples to include application name
    Former-commit-id: 490a12997db2d35bf0639af81ff42641449bbbd9

--- ArcGISRuntimeSDKQt_CppSamples/LocalServer/LocalServerGeoprocessing/main.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/LocalServer/LocalServerGeoprocessing/main.cpp
@@ -30,6 +30,7 @@
 int main(int argc, char *argv[])
 {
   QGuiApplication app(argc, argv);
+  app.setApplicationName("LocalServerGeoprocessing - C++");
 
   // Initialize the sample
   LocalServerGeoprocessing::init();
