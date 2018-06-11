5c46d716911d3f7bc8deda81ee248ed0a5051fbf
Andrew Morris <amorris@esri.com>
2018-06-11 11:34:50 -0700

    C++ -> QML label fixes

--- ArcGISRuntimeSDKQt_QMLSamples/Routing/ServiceArea/main.cpp
+++ ArcGISRuntimeSDKQt_QMLSamples/Routing/ServiceArea/main.cpp
@@ -24,7 +24,7 @@
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
-  app.setApplicationName("ServiceArea - C++");
+    app.setApplicationName("ServiceArea - QML");
 
     // Initialize application view
     QQuickView view;
