722ceda142c9174655c6b8713f92ab929fb5a9fe
Andrew Morris <amorris@esri.com>
2018-06-11 11:12:49 -0700

    "C++" -> "QML" label fixes

--- ArcGISRuntimeSDKQt_QMLSamples/Geometry/FormatCoordinates/main.cpp
+++ ArcGISRuntimeSDKQt_QMLSamples/Geometry/FormatCoordinates/main.cpp
@@ -24,7 +24,7 @@
 int main(int argc, char *argv[])
 {
   QGuiApplication app(argc, argv);
-  app.setApplicationName("FormatCoordinates - C++");
+  app.setApplicationName("FormatCoordinates - QML");
 
   // Initialize application view
   QQuickView view;
