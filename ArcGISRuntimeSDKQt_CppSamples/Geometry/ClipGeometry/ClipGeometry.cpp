bf40541305cbd81d4e5c4795d6408b18c9964577
Andrew Morris <amorris@esri.com>
2018-07-03 11:14:28 -0700

    clipGeometry (C++)

---
+++ ArcGISRuntimeSDKQt_CppSamples/Geometry/ClipGeometry/ClipGeometry.cpp
@@ -0,0 +1,128 @@
+// [WriteFile Name=ClipGeometry, Category=Geometry]
+// [Legal]
+// Copyright 2018 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#include "ClipGeometry.h"
+
+#include "Map.h"
+#include "MapQuickView.h"
+#include "Geometry.h"
+#include "Graphic.h"
+#include "GeometryEngine.h"
+#include "GraphicsOverlay.h"
+#include "SimpleLineSymbol.h"
+#include "SimpleFillSymbol.h"
+
+using namespace Esri::ArcGISRuntime;
+
+ClipGeometry::ClipGeometry(QQuickItem* parent /* = nullptr */):
+  QQuickItem(parent)
+{
+}
+
+void ClipGeometry::init()
+{
+  // Register the map view for QML
+  qmlRegisterType<MapQuickView>("Esri.Samples", 1, 0, "MapView");
+  qmlRegisterType<ClipGeometry>("Esri.Samples", 1, 0, "ClipGeometrySample");
+}
+
+void ClipGeometry::componentComplete()
+{
+  QQuickItem::componentComplete();
+
+  // find QML MapView component
+  m_mapView = findChild<MapQuickView*>("mapView");
+
+  // Create a map using the topographic basemap
+  m_map = new Map(Basemap::topographic(this), this);
+
+  // Create a graphics overlay for the Colorado graphic
+  m_coloradoOverlay = new GraphicsOverlay(this);
+
+  m_envelopesOverlay = new GraphicsOverlay(this);
+
+  m_clippedAreasOverlay = new GraphicsOverlay(this);
+
+  // Create an envelope to be used as the Colorado geometry
+  m_coloradoEnvelope = Envelope(-12138232.018408, 4441198.773776, -11362327.128340, 5012861.290274, SpatialReference(3857));
+
+  // Create an envelope for each clipping area
+  m_outsideEnvelope = Envelope(-12201990.219681, 5147942.225174, -11858344.321294, 5297071.577304, SpatialReference(3857));
+  m_intersectingEnvelope = Envelope(-12260345.183558, 4332053.378376, -11962086.479298, 4566553.881363, SpatialReference(3857));
+  m_insideEnvelope = Envelope(-11655182.595204, 4593570.068343, -11431488.567009, 4741618.772994, SpatialReference(3857));
+
+  // Set map to map view and append the two initial graphics overlays
+  m_mapView->setMap(m_map);
+  m_mapView->graphicsOverlays()->append(m_coloradoOverlay);
+  m_mapView->graphicsOverlays()->append(m_envelopesOverlay);
+
+  // Set initialViewpoint
+  Viewpoint initialViewpoint(Envelope(-12500000, 4000000, -11000000, 5500000, SpatialReference(3857)));
+  m_mapView->setViewpoint(initialViewpoint);
+
+  // Create the graphics
+  createGraphics();
+
+}
+
+// Creates the visual elements and adds them to their respective overlays
+void ClipGeometry::createGraphics(){
+
+  // Colorado outline + fill
+  m_coloradoOutline = new SimpleLineSymbol(SimpleLineSymbolStyle::Solid, QColor("#FF0000FF"), 2.0f, this);
+  m_coloradoFill = new SimpleFillSymbol(SimpleFillSymbolStyle::Solid, QColor("#220000FF"), m_coloradoOutline, this);
+
+  // Create and add the Colorado graphic to its overlay
+  m_coloradoGraphic = new Graphic(m_coloradoEnvelope, m_coloradoFill, this);
+  m_coloradoOverlay->graphics()->append(m_coloradoGraphic);
+
+  // Clipping envelopes outline
+  SimpleLineSymbol* envelopeOutline = new SimpleLineSymbol(SimpleLineSymbolStyle::Dash, QColor("#FFFF0000"), 3.0f, this);
+
+  // Create and add the clipping envelope graphics to their overlay
+  m_outsideGraphic = new Graphic(m_outsideEnvelope, envelopeOutline, this);
+  m_intersectingGraphic = new Graphic(m_intersectingEnvelope, envelopeOutline, this);
+  m_insideGraphic = new Graphic(m_insideEnvelope, envelopeOutline, this);
+  m_envelopesOverlay->graphics()->append(m_outsideGraphic);
+  m_envelopesOverlay->graphics()->append(m_intersectingGraphic);
+  m_envelopesOverlay->graphics()->append(m_insideGraphic);
+
+}
+
+// Clips the overlapping geometries and displays them on the map
+void ClipGeometry::clipAreas(){
+
+  // Immediately hide the reference graphic
+  m_coloradoGraphic->setVisible(false);
+
+  // Get the graphicsListModel from the overlay containing the clipping envelopes
+  GraphicListModel* graphics = m_envelopesOverlay->graphics();
+
+  /* Iterate through the envelope graphics and call the static 'clip' function, passing in
+   * the reference graphic and the current iteration's envelope geometry as arguments.
+   * If the return value is valid, create a new graphic using the returned geometry and the
+   * fillSymbol of the reference graphic as arguments. Append the new graphic to its respective
+   * graphicsOverlay. */
+  for(int i = 0; i < graphics->size(); i++){
+    m_clippedGeometry = GeometryEngine::clip(m_coloradoGraphic->geometry(), graphics->at(i)->geometry().extent());
+    if(!m_clippedGeometry.isEmpty()){
+      Graphic* clippedGraphic = new Graphic(m_clippedGeometry, m_coloradoFill, this);
+      m_clippedAreasOverlay->graphics()->append(clippedGraphic);
+    }
+  }
+  // Append the graphicsOverlay to the mapView to render
+  m_mapView->graphicsOverlays()->append(m_clippedAreasOverlay);
+}
---
+++ ArcGISRuntimeSDKQt_CppSamples/Geometry/ClipGeometry/ClipGeometry.h
@@ -0,0 +1,77 @@
+// [WriteFile Name=ClipGeometry, Category=Geometry]
+// [Legal]
+// Copyright 2018 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#ifndef CLIPGEOMETRY_H
+#define CLIPGEOMETRY_H
+
+namespace Esri
+{
+namespace ArcGISRuntime
+{
+class Map;
+class MapQuickView;
+class Graphic;
+class GraphicsOverlay;
+class SimpleLineSymbol;
+class SimpleFillSymbol;
+class Envelope;
+}
+}
+
+#include <QQuickItem>
+#include "Envelope.h"
+#include "SimpleFillSymbol.h"
+#include "SimpleLineSymbol.h"
+
+class ClipGeometry : public QQuickItem
+{
+  Q_OBJECT
+
+public:
+  explicit ClipGeometry(QQuickItem* parent = nullptr);
+  ~ClipGeometry() = default;
+
+  void componentComplete() Q_DECL_OVERRIDE;
+  static void init();
+  Q_INVOKABLE void clipAreas();
+
+private:
+  void createGraphics();
+
+private:
+  Esri::ArcGISRuntime::Map* m_map = nullptr;
+  Esri::ArcGISRuntime::MapQuickView* m_mapView = nullptr;
+  Esri::ArcGISRuntime::Graphic* m_coloradoGraphic = nullptr;
+  Esri::ArcGISRuntime::Graphic* m_outsideGraphic = nullptr;
+  Esri::ArcGISRuntime::Graphic* m_intersectingGraphic = nullptr;
+  Esri::ArcGISRuntime::Graphic* m_insideGraphic = nullptr;
+  Esri::ArcGISRuntime::Graphic* clippedGraphic = nullptr;
+  Esri::ArcGISRuntime::GraphicsOverlay* m_coloradoOverlay = nullptr;
+  Esri::ArcGISRuntime::GraphicsOverlay* m_envelopesOverlay = nullptr;
+  Esri::ArcGISRuntime::GraphicsOverlay* m_clippedAreasOverlay = nullptr;
+  Esri::ArcGISRuntime::Geometry m_clippedGeometry;
+  Esri::ArcGISRuntime::Envelope m_coloradoEnvelope;
+  Esri::ArcGISRuntime::Envelope m_outsideEnvelope;
+  Esri::ArcGISRuntime::Envelope m_insideEnvelope;
+  Esri::ArcGISRuntime::Envelope m_intersectingEnvelope;
+  Esri::ArcGISRuntime::SimpleFillSymbol* m_coloradoFill = nullptr;
+  Esri::ArcGISRuntime::SimpleLineSymbol* m_coloradoOutline = nullptr;
+  Esri::ArcGISRuntime::SimpleLineSymbol* envelopeOutline = nullptr;
+  Esri::ArcGISRuntime::SimpleLineSymbol* m_envelopeGraphic = nullptr;
+
+};
+
+#endif // CLIPGEOMETRY_H
