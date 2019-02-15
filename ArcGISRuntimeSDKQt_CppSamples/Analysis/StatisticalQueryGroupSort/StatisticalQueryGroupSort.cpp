11273cc6a7856d76c7b44c7e6800e6810a00e08e
jcaccamo <jcaccamo@esri.com>
2019-02-15 10:07:30 -0800

    Reverting back to using an iterator type for QVariantMap. Added const to QString object.

--- ArcGISRuntimeSDKQt_CppSamples/Analysis/StatisticalQueryGroupSort/StatisticalQueryGroupSort.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/Analysis/StatisticalQueryGroupSort/StatisticalQueryGroupSort.cpp
@@ -106,17 +106,17 @@ void StatisticalQueryGroupSort::connectSignals()
       // get the group string
       QStringList sectionStrings;
       const QVariantMap& groupsMap = record->group();
-      for (const auto& it : groupsMap.toStdMap() )
+      for (auto it = groupsMap.cbegin(); it != groupsMap.cend(); ++it)
       {
-          sectionStrings << QString("\"%1\":\"%2\"").arg(it.first, it.second.toString());
+          sectionStrings << QString("\"%1\":\"%2\"").arg(it.key(), it.value().toString());
       }
       const QString sectionString = sectionStrings.join(',');
 
       // obtain the statistics
       const QVariantMap& statsMap = record->statistics();
-      for (const auto& it : statsMap.toStdMap())
+      for (auto it = statsMap.cbegin(); it != statsMap.cend(); ++it)
       {
-        QString statString = QString("%1: %2").arg(it.first, it.second.toString());
+        const QString statString = QString("%1: %2").arg(it.key(), it.value().toString());
         addResultToModel(sectionString, statString);
       }
     }
