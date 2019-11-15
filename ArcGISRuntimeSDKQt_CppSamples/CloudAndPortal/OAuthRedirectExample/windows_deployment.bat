063082d2f780e12e7ade31c9d1d59a6a6dbe1c76
James Ballard <jballard@esri.com>
2019-11-14 16:10:27 -0800

    Update the version to 5.12.6.

--- ArcGISRuntimeSDKQt_CppSamples/CloudAndPortal/OAuthRedirectExample/windows_deployment.bat
+++ ArcGISRuntimeSDKQt_CppSamples/CloudAndPortal/OAuthRedirectExample/windows_deployment.bat
@@ -2,10 +2,10 @@ ECHO OFF
 
 SET "ARCH=x64"
 SET "VC_ARCH=amd64"
-SET "QT_KIT=C:\Qt\5.12.0\msvc2017_64"
+SET "QT_KIT=C:\Qt\5.12.6\msvc2017_64"
 SET "DIR=%~dp0"
 SET "BUILD_TYPE=release"
-SET "BUILD_DIR=%DIR%..\build-OAuthRedirectExample-Desktop_Qt_5_12_0_MSVC2017_64bit-Release\%BUILD_TYPE%"
+SET "BUILD_DIR=%DIR%..\build-OAuthRedirectExample-Desktop_Qt_5_12_6_MSVC2017_64bit-Release\%BUILD_TYPE%"
 SET "ARCGIS_SDK_DIR=C:\Program Files (x86)\ArcGIS SDKs\Qt100.7\sdk\windows\%ARCH%\bin\%BUILD_TYPE%"
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvarsall.bat" %VC_ARCH%
