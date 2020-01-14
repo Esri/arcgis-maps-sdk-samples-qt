cd5756535c2d6ab58941805c99a905fd604188b3
Jared Caccamo <jcaccamo@esri.com>
2020-01-14 08:58:21 -0800

    initial commit, c++ flyover ar working, just needs cleaning

---
+++ ArcGISRuntimeSDKQt_CppSamples/Scenes/ExploreScenesInFlyoverAR/ExploreScenesInFlyoverAR.cpp
@@ -0,0 +1,106 @@
+// [WriteFile Name=ExploreScenesInFlyoverAR, Category=Scenes]
+// [Legal]
+// Copyright 2019 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#ifdef PCH_BUILD
+#include "pch.hpp"
+#endif // PCH_BUILD
+
+#include "ExploreScenesInFlyoverAR.h"
+
+#include "ArcGISTiledElevationSource.h"
+#include "Scene.h"
+#include "SceneQuickView.h"
+#include "IntegratedMeshLayer.h"
+
+using namespace Esri::ArcGISRuntime;
+using namespace Esri::ArcGISRuntime::Toolkit;
+
+ExploreScenesInFlyoverAR::ExploreScenesInFlyoverAR(QObject* parent /* = nullptr */):
+  QObject(parent),
+  m_scene(new Scene(Basemap::imagery(this), this))
+{
+  // create a new elevation source from Terrain3D REST service
+  ArcGISTiledElevationSource* elevationSource = new ArcGISTiledElevationSource(
+        QUrl("https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"), this);
+
+  // add the elevation source to the scene to display elevation
+  m_scene->baseSurface()->elevationSources()->append(elevationSource);
+
+  // create the integrated mesh layer
+  const QUrl meshLyrUrl("https://www.arcgis.com/home/item.html?id=dbc72b3ebb024c848d89a42fe6387a1b");
+  m_integratedMeshLayer = new IntegratedMeshLayer(meshLyrUrl, this);
+
+  // add the layer to the scene
+  m_scene->operationalLayers()->append(m_integratedMeshLayer);
+
+  connect(m_integratedMeshLayer, &IntegratedMeshLayer::doneLoading, this, [this](Error e)
+  {
+    if (!e.isEmpty())
+    {
+      qDebug() << e.code() << e.message() << " - " << e.additionalMessage();
+      return;
+    }
+
+    m_scene->baseSurface()->setNavigationConstraint(NavigationConstraint::None);
+    m_originCamera = Camera(m_integratedMeshLayer->fullExtent().center().y(), m_integratedMeshLayer->fullExtent().center().x(), 250, 0, 90, 0);
+    arcGISArView()->setOriginCamera(m_originCamera);
+    arcGISArView()->setTranslationFactor(1000);
+    emit arcGISArViewChanged();
+  });
+}
+
+ExploreScenesInFlyoverAR::~ExploreScenesInFlyoverAR() = default;
+
+void ExploreScenesInFlyoverAR::init()
+{
+  // Register classes for QML
+  qmlRegisterType<SceneQuickView>("Esri.Samples", 1, 0, "SceneView");
+  qmlRegisterType<ExploreScenesInFlyoverAR>("Esri.Samples", 1, 0, "ExploreScenesInFlyoverARSample");
+}
+
+SceneQuickView* ExploreScenesInFlyoverAR::sceneView() const
+{
+  return m_sceneView;
+}
+
+ArcGISArView* ExploreScenesInFlyoverAR::arcGISArView() const
+{
+  return m_arcGISArView;
+}
+
+// Set the view (created in QML)
+void ExploreScenesInFlyoverAR::setSceneView(SceneQuickView* sceneView)
+{
+  if (!sceneView || sceneView == m_sceneView)
+    return;
+
+  m_sceneView = sceneView;
+  m_sceneView->setArcGISScene(m_scene);
+
+  emit sceneViewChanged();
+}
+
+// Set the AR view
+void ExploreScenesInFlyoverAR::setArcGISArView(ArcGISArView* arcGISArView)
+{
+  if (!arcGISArView || arcGISArView == m_arcGISArView)
+    return;
+
+  m_arcGISArView = arcGISArView;
+
+  emit arcGISArViewChanged();
+}
+
---
+++ ArcGISRuntimeSDKQt_CppSamples/Scenes/ExploreScenesInFlyoverAR/ExploreScenesInFlyoverAR.pro
@@ -0,0 +1,79 @@
+#-------------------------------------------------
+# Copyright 2019 Esri.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#-------------------------------------------------
+
+mac {
+    cache()
+}
+
+#-------------------------------------------------------------------------------
+
+CONFIG += c++14
+
+# additional modules are pulled in via arcgisruntime.pri
+QT += opengl qml quick
+
+TEMPLATE = app
+TARGET = ExploreScenesInFlyoverAR
+
+ARCGIS_RUNTIME_VERSION = 100.8
+include($$PWD/arcgisruntime.pri)
+
+#-------------------------------------------------------------------------------
+
+HEADERS += \
+    ExploreScenesInFlyoverAR.h
+
+SOURCES += \
+    main.cpp \
+    ExploreScenesInFlyoverAR.cpp
+
+RESOURCES += ExploreScenesInFlyoverAR.qrc
+
+#-------------------------------------------------------------------------------
+
+win32 {
+    LIBS += \
+        Ole32.lib
+}
+
+ios {
+    INCLUDEPATH += $$PWD
+    DEPENDPATH += $$PWD
+
+    OTHER_FILES += \
+        $$PWD/Info.plist
+
+    QMAKE_INFO_PLIST = $$PWD/Info.plist
+}
+
+android {
+    INCLUDEPATH += $$PWD
+    DEPENDPATH += $$PWD
+}
+
+#-------------------------------------------------------------------------------
+# AR configuration
+
+# The path to the ArcGIS runtime toolkit for Qt sources, corresponding to the files downloaded
+# from the GitHub repo: https://github.com/Esri/arcgis-runtime-toolkit-qt
+
+AR_TOOLKIT_SOURCE_PATH = /Users/jare8800/Applications/arvr # must be set to the path to toolkit sources
+
+isEmpty(AR_TOOLKIT_SOURCE_PATH) {
+    error(AR_TOOLKIT_SOURCE_PATH is not set)
+}
+
+include($$AR_TOOLKIT_SOURCE_PATH/Plugin/CppApi/ArCppApi.pri)
+#-------------------------------------------------------------------------------
