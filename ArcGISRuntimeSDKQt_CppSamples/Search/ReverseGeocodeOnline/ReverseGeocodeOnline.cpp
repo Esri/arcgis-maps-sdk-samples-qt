4cf76236843011c1fd7cb1512eb384db7972b0d4
sbiswas <sbiswas@esri.com>
2020-02-18 12:37:01 +0000

    initial commit C++

---
+++ ArcGISRuntimeSDKQt_CppSamples/Search/ReverseGeocodeOnline/ReverseGeocodeOnline.cpp
@@ -0,0 +1,76 @@
+// [WriteFile Name=ReverseGeocodeOnline, Category=Search]
+// [Legal]
+// Copyright 2020 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#ifdef PCH_BUILD
+#include "pch.hpp"
+#endif // PCH_BUILD
+
+#include "ReverseGeocodeOnline.h"
+
+#include "CalloutData.h"
+#include "Graphic.h"
+#include "GraphicsOverlay.h"
+#include "LocatorTask.h"
+#include "Map.h"
+#include "MapQuickView.h"
+
+#include <QUrl>
+
+using namespace Esri::ArcGISRuntime;
+
+ReverseGeocodeOnline::ReverseGeocodeOnline(QObject* parent /* = nullptr */):
+  QObject(parent),
+  m_map(new Map(Basemap::imageryWithLabels(this), this))
+{
+//  GraphicsOverlay* graphicsOverlay = new GraphicsOverlay(this);
+
+  const QUrl url("http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer");
+  LocatorTask* locatorTask = new LocatorTask(url, this);
+
+//  m_mapView->graphicsOverlays()->append(graphicsOverlay);
+}
+
+ReverseGeocodeOnline::~ReverseGeocodeOnline() = default;
+
+void ReverseGeocodeOnline::init()
+{
+  // Register the map view for QML
+  qmlRegisterType<MapQuickView>("Esri.Samples", 1, 0, "MapView");
+  qmlRegisterType<ReverseGeocodeOnline>("Esri.Samples", 1, 0, "ReverseGeocodeOnlineSample");
+  qmlRegisterUncreatableType<CalloutData>("Esri.Samples", 1, 0, "CalloutData", "CalloutData is an uncreatable type");
+}
+
+MapQuickView* ReverseGeocodeOnline::mapView() const
+{
+  return m_mapView;
+}
+
+CalloutData* ReverseGeocodeOnline::calloutData() const
+{
+  return m_calloutData;
+}
+
+// Set the view (created in QML)
+void ReverseGeocodeOnline::setMapView(MapQuickView* mapView)
+{
+  if (!mapView || mapView == m_mapView)
+    return;
+
+  m_mapView = mapView;
+  m_mapView->setMap(m_map);
+
+  emit mapViewChanged();
+}
