eb55f648e8b07ff064eb944bfc67b1d48948c55f
sbiswas <sbiswas@esri.com>
2020-03-13 15:01:10 +0000

    incomplete- updating data for ShowLabelsonLayer, JSONs not correct yet

--- ArcGISRuntimeSDKQt_CppSamples/DisplayInformation/ShowLabelsOnLayers/ShowLabelsOnLayers.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/DisplayInformation/ShowLabelsOnLayers/ShowLabelsOnLayers.cpp
@@ -51,7 +51,8 @@ void ShowLabelsOnLayers::componentComplete()
   m_map = new Map(Basemap::lightGrayCanvas(this), this);
 
   // Create a feature layer
-  ServiceFeatureTable* featureTable = new ServiceFeatureTable(QUrl("https://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/1"), this);
+//  ServiceFeatureTable* featureTable = new ServiceFeatureTable(QUrl("http://sampleserver6.arcgisonline.com/arcgis/rest/services/USA/MapServer/1"), this);
+  ServiceFeatureTable* featureTable = new ServiceFeatureTable(QUrl("https://services.arcgis.com/P3ePLMYs2RVChkJx/arcgis/rest/services/USA_115th_Congressional_Districts/FeatureServer/0"), this);
   FeatureLayer* featureLayer = new FeatureLayer(featureTable, this);
   connect(featureLayer, &FeatureLayer::doneLoading, [this, featureLayer](Error e)
   {
@@ -69,6 +70,7 @@ void ShowLabelsOnLayers::componentComplete()
   featureLayer->setLabelsEnabled(true);
 
   // Set map to map view
+  m_mapView->setViewpointCenter(Point(-10846309.950860, 4683272.219411, SpatialReference::webMercator()), 20000000);
   m_mapView->setMap(m_map);
 }
 
@@ -84,5 +86,8 @@ QString ShowLabelsOnLayers::createLabelJson() const
   // (3) The 'where' clause restricts the labels to be displayed that has valid (non-empty) data. Empty data
   //     for this service has a single blank space in the 'rte_num1' field.
   // (4) The 'symbol' for the labeled text will be blue with a yellow halo.
-  return QString(QStringLiteral("{\"labelExpressionInfo\":{\"expression\":\"'I - ' + $feature.rte_num1\"},\"labelPlacement\":\"esriServerLinePlacementAboveAlong\",\"where\":\"rte_num1 <> ' '\",\"symbol\": {\"angle\":0,\"backgroundColor\":[0,0,0,0],\"borderLineColor\":[0,0,0,0],\"borderLineSize\":0,\"color\":[0,0,255,255],\"font\": {\"decoration\":\"none\",\"size\":15,\"style\":\"normal\",\"weight\":\"normal\"},\"haloColor\":[255,255,0,255],\"haloSize\":1.5,\"horizontalAlignment\":\"center\",\"kerning\":false,\"type\":\"esriTS\",\"verticalAlignment\":\"middle\",\"xoffset\":0,\"yoffset\":0}}"));
+  QString republicanJson = QString(QStringLiteral("{\"labelExpressionInfo\":{\"expression\":$feature.NAME + \" (\" + left($feature.PARTY,1) + \")\\nDistrict \" + $feature.CDFIPS\"},\"labelPlacement\":\"esriServerPolygonPlacementAlwaysHorizontal\",\"where\":\"PARTY = 'Republican'\",\"symbol\": {\"angle\":0,\"backgroundColor\":[0,0,0,0],\"borderLineColor\":[0,0,0,0],\"borderLineSize\":0,\"color\":[255,0,0,255],\"font\": {\"decoration\":\"none\",\"size\":10,\"style\":\"normal\",\"weight\":\"normal\"},\"haloColor\":[255,255,255,255],\"haloSize\":1.5,\"horizontalAlignment\":\"center\",\"kerning\":false,\"type\":\"esriTS\",\"verticalAlignment\":\"middle\",\"xoffset\":0,\"yoffset\":0}}"));
+  QString democratJson = QString(QStringLiteral("{\"labelExpressionInfo\":{\"expression\":\"'I - ' + $feature.rte_num1\"},\"labelPlacement\":\"esriServerLinePlacementAboveAlong\",\"where\":\"rte_num1 <> ' '\",\"symbol\": {\"angle\":0,\"backgroundColor\":[0,0,0,0],\"borderLineColor\":[0,0,0,0],\"borderLineSize\":0,\"color\":[0,0,255,255],\"font\": {\"decoration\":\"none\",\"size\":10,\"style\":\"normal\",\"weight\":\"normal\"},\"haloColor\":[255,255,255,255],\"haloSize\":1.5,\"horizontalAlignment\":\"center\",\"kerning\":false,\"type\":\"esriTS\",\"verticalAlignment\":\"middle\",\"xoffset\":0,\"yoffset\":0}}"));
+  return republicanJson;
+//  return QString(QStringLiteral("{\"labelExpressionInfo\":{\"expression\":\"'I - ' + $feature.rte_num1\"},\"labelPlacement\":\"esriServerLinePlacementAboveAlong\",\"where\":\"rte_num1 <> ' '\",\"symbol\": {\"angle\":0,\"backgroundColor\":[0,0,0,0],\"borderLineColor\":[0,0,0,0],\"borderLineSize\":0,\"color\":[0,0,255,255],\"font\": {\"decoration\":\"none\",\"size\":15,\"style\":\"normal\",\"weight\":\"normal\"},\"haloColor\":[255,255,0,255],\"haloSize\":1.5,\"horizontalAlignment\":\"center\",\"kerning\":false,\"type\":\"esriTS\",\"verticalAlignment\":\"middle\",\"xoffset\":0,\"yoffset\":0}}"));
 }
