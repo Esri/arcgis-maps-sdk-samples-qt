5345e5046aa502fce448c5a9308690fbb5349f35
Guillaume Belz <1651508+GuillaumeBelz@users.noreply.github.com>
2020-11-23 17:02:20 -0800

    Update ListRelatedFeatures.h

--- ArcGISRuntimeSDKQt_CppSamples/Features/ListRelatedFeatures/ListRelatedFeatures.h
+++ ArcGISRuntimeSDKQt_CppSamples/Features/ListRelatedFeatures/ListRelatedFeatures.h
@@ -57,6 +57,7 @@ private:
   Esri::ArcGISRuntime::MapQuickView* m_mapView = nullptr;
   Esri::ArcGISRuntime::FeatureLayer* m_alaskaNationalParks = nullptr;  
   Esri::ArcGISRuntime::ArcGISFeatureTable* m_alaskaFeatureTable = nullptr;
+  Esri::ArcGISRuntime::ArcGISFeature* m_selectedFeature = nullptr;
   RelatedFeatureListModel* m_relatedFeaturesModel = nullptr;
 };
 
