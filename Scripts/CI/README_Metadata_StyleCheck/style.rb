fa212a62f4ae97159a5530c1d551842d7222b987
tanneryould <tyould@esri.com>
2020-11-23 16:57:37 -0800

    Add Apache License

--- Scripts/CI/README_Metadata_StyleCheck/style.rb
+++ Scripts/CI/README_Metadata_StyleCheck/style.rb
@@ -1,3 +1,16 @@
+# Copyright 2020 Esri.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 all  # opt-in all rules by default
 rule 'MD003', :style => :atx  # Header style written as non-closing pound marks. e.g. ## Section title
 rule 'MD004', :style => :asterisk  # Unordered list style as asterisk, rather than hyphen or plus sign
