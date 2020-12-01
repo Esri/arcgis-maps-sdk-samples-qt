c24020ea4e36f7ee20846fd15894fdcc377797ea
Jared Caccamo <jcaccamo@esri.com>
2020-12-01 11:11:40 -0800

    undoing api key code as it will be part of a later update.

--- ArcGISRuntimeSDKQt_CppSamples/Scenes/BasicSceneView/main.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/Scenes/BasicSceneView/main.cpp
@@ -46,24 +46,6 @@ int main(int argc, char *argv[])
   QGuiApplication app(argc, argv);
   app.setApplicationName("BasicSceneView - C++");
 
-  // Use of Esri location services, including basemaps and geocoding,
-  // requires authentication using either an ArcGIS identity or an API Key.
-  // 1. ArcGIS identity: An ArcGIS named user account that is a member of an
-  //    organization in ArcGIS Online or ArcGIS Enterprise.
-  // 2. API key: A permanent key that gives your application access to Esri
-  //    location services. Visit your ArcGIS Developers Dashboard create a new
-  //    API keys or access an existing API key.
-  const QString apiKey = "";
-  if (apiKey.isEmpty())
-  {
-      qWarning() << "Use of Esri location services, including basemaps, requires"
-                    "you to authenticate with an ArcGIS identity or set the API Key property.";
-  }
-  else
-  {
-      Esri::ArcGISRuntime::ArcGISRuntimeEnvironment::setApiKey(apiKey);
-  }
-
   // Initialize the sample
   BasicSceneView::init();
 
@@ -73,7 +55,7 @@ int main(int argc, char *argv[])
 
   // Add the import Path
   view.engine()->addImportPath(QDir(QCoreApplication::applicationDirPath()).filePath("qml"));
-  
+
   QString arcGISRuntimeImportPath = QUOTE(ARCGIS_RUNTIME_IMPORT_PATH);
 
  #if defined(LINUX_PLATFORM_REPLACEMENT)
