dd60c4a109fdffcbaad7cf950ab7824fcf8fbfb2
Tanner Yould <tyould@esri.com>
2020-12-02 08:49:05 -0800

    Revert "Should we add this to every main.cpp?"
    This reverts commit 4b88991ba291c24d38dc3d20a3758eb9618b6e02.

--- ArcGISRuntimeSDKQt_CppSamples/Layers/FeatureLayerRenderingModeMap/main.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/Layers/FeatureLayerRenderingModeMap/main.cpp
@@ -23,7 +23,6 @@
 #endif
 
 #include "FeatureLayerRenderingModeMap.h"
-#include "ArcGISRuntimeEnvironment.h"
 
 #define STRINGIZE(x) #x
 #define QUOTE(x) STRINGIZE(x)
@@ -39,25 +38,6 @@ int main(int argc, char *argv[])
 #endif
   QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QGuiApplication app(argc, argv);
-  app.setApplicationName("FeatureLayerRenderingModeMap - C++");
-
-  // Use of Esri location services, including basemaps and geocoding,
-  // requires authentication using either an ArcGIS identity or an API Key.
-  // 1. ArcGIS identity: An ArcGIS named user account that is a member of an
-  //    organization in ArcGIS Online or ArcGIS Enterprise.
-  // 2. API key: A permanent key that gives your application access to Esri
-  //    location services. Visit your ArcGIS Developers Dashboard create a new
-  //    API keys or access an existing API key.
-  const QString apiKey = "";
-  if (apiKey.isEmpty())
-  {
-      qWarning() << "Use of Esri location services, including basemaps, requires"
-                    "you to authenticate with an ArcGIS identity or set the API Key property.";
-  }
-  else
-  {
-      Esri::ArcGISRuntime::ArcGISRuntimeEnvironment::setApiKey(apiKey);
-  }
   app.setApplicationName("FeatureLayerRenderingModeMap - C++");
 
   // Initialize the sample
