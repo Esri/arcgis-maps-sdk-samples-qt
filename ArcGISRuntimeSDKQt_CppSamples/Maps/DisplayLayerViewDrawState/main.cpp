cea3edabd04b6711cd151afae2bff18b8b03fc70
Tanner Yould <tyould@esri.com>
2020-12-08 11:30:50 -0800

    Commit cpp samples 1

--- ArcGISRuntimeSDKQt_CppSamples/Maps/DisplayLayerViewDrawState/main.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/Maps/DisplayLayerViewDrawState/main.cpp
@@ -16,6 +16,7 @@
 #endif
 
 #include "DisplayLayerViewDrawState.h"
+#include "ArcGISRuntimeEnvironment.h"
 
 #include <QDir>
 #include <QGuiApplication>
@@ -28,6 +29,24 @@ int main(int argc, char *argv[])
   QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QGuiApplication app(argc, argv);
   app.setApplicationName(QStringLiteral("DisplayLayerViewDrawState - C++"));
+
+  // Use of Esri location services, including basemaps and geocoding,
+  // requires authentication using either an ArcGIS identity or an API Key.
+  // 1. ArcGIS identity: An ArcGIS named user account that is a member of an
+  //    organization in ArcGIS Online or ArcGIS Enterprise.
+  // 2. API key: A permanent key that gives your application access to Esri
+  //    location services. Visit your ArcGIS Developers Dashboard create a new
+  //    API keys or access an existing API key.
+  const QString apiKey = "";
+  if (apiKey.isEmpty())
+  {
+      qWarning() << "Use of Esri location services, including basemaps, requires"
+                    "you to authenticate with an ArcGIS identity or set the API Key property.";
+  }
+  else
+  {
+      Esri::ArcGISRuntime::ArcGISRuntimeEnvironment::setApiKey(apiKey);
+  }
 
   // Initialize the sample
   DisplayLayerViewDrawState::init();
--- ArcGISRuntimeSDKQt_CppSamples/Scenes/OpenMobileScenePackage/main.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/Scenes/OpenMobileScenePackage/main.cpp
@@ -12,6 +12,7 @@
 // limitations under the License.
 
 #include "OpenMobileScenePackage.h"
+#include "ArcGISRuntimeEnvironment.h"
 
 #include <QDir>
 #include <QGuiApplication>
@@ -39,6 +40,24 @@ int main(int argc, char *argv[])
   QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
   QGuiApplication app(argc, argv);
   app.setApplicationName(QStringLiteral("OpenMobileScenePackage - C++"));
+
+  // Use of Esri location services, including basemaps and geocoding,
+  // requires authentication using either an ArcGIS identity or an API Key.
+  // 1. ArcGIS identity: An ArcGIS named user account that is a member of an
+  //    organization in ArcGIS Online or ArcGIS Enterprise.
+  // 2. API key: A permanent key that gives your application access to Esri
+  //    location services. Visit your ArcGIS Developers Dashboard create a new
+  //    API keys or access an existing API key.
+  const QString apiKey = "";
+  if (apiKey.isEmpty())
+  {
+      qWarning() << "Use of Esri location services, including basemaps, requires"
+                    "you to authenticate with an ArcGIS identity or set the API Key property.";
+  }
+  else
+  {
+      Esri::ArcGISRuntime::ArcGISRuntimeEnvironment::setApiKey(apiKey);
+  }
 
   // Initialize the sample
   OpenMobileScenePackage::init();
