1f6fe125d77a6e6de89bdfdf98d7d6bc57d7aad5
Tanner Yould <tyould@esri.com>
2021-02-23 13:51:41 -0800

    Setup map and viewpoint

--- ArcGISRuntimeSDKQt_CppSamples/DisplayInformation/CreateSymbolStylesFromWebStyles/CreateSymbolStylesFromWebStyles.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/DisplayInformation/CreateSymbolStylesFromWebStyles/CreateSymbolStylesFromWebStyles.cpp
@@ -17,14 +17,22 @@
 #include "Map.h"
 #include "MapQuickView.h"
 
+#include "FeatureLayer.h"
+#include "ServiceFeatureTable.h"
+
 #include <QUrl>
 
 using namespace Esri::ArcGISRuntime;
 
 CreateSymbolStylesFromWebStyles::CreateSymbolStylesFromWebStyles(QObject* parent /* = nullptr */):
   QObject(parent),
-  m_map(new Map(BasemapStyle::ArcGISStreets, this))
+  m_map(new Map(BasemapStyle::ArcGISNavigation, this))
 {
+  m_map->setReferenceScale(100'000);
+
+  QUrl webStyleLayerUrl = QUrl("http://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/LA_County_Points_of_Interest/FeatureServer/0");
+  FeatureLayer* webStyleLayer = new FeatureLayer(new ServiceFeatureTable(webStyleLayerUrl, this), this);
+  m_map->operationalLayers()->append(webStyleLayer);
 }
 
 CreateSymbolStylesFromWebStyles::~CreateSymbolStylesFromWebStyles()
@@ -46,6 +54,13 @@ void CreateSymbolStylesFromWebStyles::setMapView(MapQuickView* mapView)
 
   m_mapView = mapView;
   m_mapView->setMap(m_map);
+
+  const double x_longitude = -118.44186;
+  const double y_latitude = 34.38301;
+  const Point centerPt(x_longitude, y_latitude, SpatialReference::wgs84());
+  const double scale = 7000;
+
+  m_mapView->setViewpointCenter(centerPt, scale);
 
   emit mapViewChanged();
 }
