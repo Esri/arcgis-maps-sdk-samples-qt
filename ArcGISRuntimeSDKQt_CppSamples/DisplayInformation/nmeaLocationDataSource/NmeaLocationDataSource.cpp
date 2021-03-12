334c5d5221e278acdfe46be4faa42b26238abb7c
Tanner Yould <tyould@esri.com>
2021-03-12 11:49:53 -0800

    Initial C++ commit

---
+++ ArcGISRuntimeSDKQt_CppSamples/DisplayInformation/nmeaLocationDataSource/NmeaLocationDataSource.cpp
@@ -0,0 +1,59 @@
+// [WriteFile Name=NmeaLocationDataSource, Category=DisplayInformation]
+// [Legal]
+// Copyright 2020 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#ifdef PCH_BUILD
+#include "pch.hpp"
+#endif // PCH_BUILD
+
+#include "NmeaLocationDataSource.h"
+
+#include "Map.h"
+#include "MapQuickView.h"
+
+using namespace Esri::ArcGISRuntime;
+
+NmeaLocationDataSource::NmeaLocationDataSource(QObject* parent /* = nullptr */):
+  QObject(parent),
+  m_map(new Map(Basemap::imagery(this), this))
+{
+
+}
+
+NmeaLocationDataSource::~NmeaLocationDataSource() = default;
+
+void NmeaLocationDataSource::init()
+{
+  // Register the map view for QML
+  qmlRegisterType<MapQuickView>("Esri.Samples", 1, 0, "MapView");
+  qmlRegisterType<NmeaLocationDataSource>("Esri.Samples", 1, 0, "NmeaLocationDataSourceSample");
+}
+
+MapQuickView* NmeaLocationDataSource::mapView() const
+{
+  return m_mapView;
+}
+
+// Set the view (created in QML)
+void NmeaLocationDataSource::setMapView(MapQuickView* mapView)
+{
+  if (!mapView || mapView == m_mapView)
+    return;
+
+  m_mapView = mapView;
+  m_mapView->setMap(m_map);
+
+  emit mapViewChanged();
+}
