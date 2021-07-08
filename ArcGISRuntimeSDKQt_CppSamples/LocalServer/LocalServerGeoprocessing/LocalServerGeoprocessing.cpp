c2d8bea8c5708cc036299a289c22d3ecf819655b
Tanner Yould <tyould@esri.com>
2021-07-08 09:52:13 -0700

    local server temp path update

--- ArcGISRuntimeSDKQt_CppSamples/LocalServer/LocalServerGeoprocessing/LocalServerGeoprocessing.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/LocalServer/LocalServerGeoprocessing/LocalServerGeoprocessing.cpp
@@ -44,11 +44,15 @@ using namespace Esri::ArcGISRuntime;
 LocalServerGeoprocessing::LocalServerGeoprocessing(QQuickItem* parent) :
   QQuickItem(parent)
 {
+  // Create a temporary directory for the local server if one has not already been created
+  if (!LocalServer::appDataPath().isEmpty() && !LocalServer::tempDataPath().isEmpty())
+    return;
+
   // create temp/data path
   const QString tempPath = LocalServerGeoprocessing::shortestTempPath() + "/EsriQtTemp";
 
   // create the directory
-  m_tempDir = std::unique_ptr<QTemporaryDir>(new QTemporaryDir(tempPath));
+  m_tempDir = std::make_unique<QTemporaryDir>(tempPath);
 
   // set the temp & app data path for the local server
   LocalServer::instance()->setTempDataPath(m_tempDir->path());
