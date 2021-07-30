adfd627987ece3a4a1395ec1a9a0d6dbe31d857b
AndrewBladon <abladon@esri.com>
2021-07-30 09:37:54 +0100

    Add the initial files for QML sample

---
+++ ArcGISRuntimeSDKQt_QMLSamples/Layers/BrowseOGCAPIFeatureService/main.cpp
@@ -0,0 +1,44 @@
+// Copyright 2021 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <QSettings>
+#include <QGuiApplication>
+#include <QQuickView>
+#include <QCommandLineParser>
+#include <QDir>
+#include <QQmlEngine>
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app(argc, argv);
+    app.setApplicationName(QStringLiteral("BrowseOGCAPIFeatureService - QML"));
+
+    // Initialize application view
+    QQuickView view;
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+
+    // Add the import Path
+    view.engine()->addImportPath(QDir(QCoreApplication::applicationDirPath()).filePath("qml"));
+
+#ifdef ARCGIS_RUNTIME_IMPORT_PATH_2
+    view.engine()->addImportPath(ARCGIS_RUNTIME_IMPORT_PATH_2);
+#endif
+
+    // Set the source
+    view.setSource(QUrl("qrc:/Samples/Layers/BrowseOGCAPIFeatureService/BrowseOGCAPIFeatureService.qml"));
+
+    view.show();
+
+    return app.exec();
+}
