6f1c7fd64dd91d7c7d7c65cad02dc4d092fc9412
AndrewBladon <abladon@esri.com>
2021-08-06 10:33:02 +0100

    Change indenting in qml sample cpp file

--- ArcGISRuntimeSDKQt_QMLSamples/Layers/BrowseOGCAPIFeatureService/main.cpp
+++ ArcGISRuntimeSDKQt_QMLSamples/Layers/BrowseOGCAPIFeatureService/main.cpp
@@ -20,43 +20,43 @@
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-    QGuiApplication app(argc, argv);
-    app.setApplicationName(QStringLiteral("BrowseOGCAPIFeatureService - QML"));
+  QGuiApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+  QGuiApplication app(argc, argv);
+  app.setApplicationName(QStringLiteral("BrowseOGCAPIFeatureService - QML"));
 
-    // Use of Esri location services, including basemaps and geocoding,
-      // requires authentication using either an ArcGIS identity or an API Key.
-      // 1. ArcGIS identity: An ArcGIS named user account that is a member of an
-      //    organization in ArcGIS Online or ArcGIS Enterprise.
-      // 2. API key: A permanent key that gives your application access to Esri
-      //    location services. Visit your ArcGIS Developers Dashboard create a new
-      //    API keys or access an existing API key.
-      const QString apiKey = QStringLiteral("");
-      if (apiKey.isEmpty())
-      {
-          qWarning() << "Use of Esri location services, including basemaps, requires"
+  // Use of Esri location services, including basemaps and geocoding,
+  // requires authentication using either an ArcGIS identity or an API Key.
+  // 1. ArcGIS identity: An ArcGIS named user account that is a member of an
+  //    organization in ArcGIS Online or ArcGIS Enterprise.
+  // 2. API key: A permanent key that gives your application access to Esri
+  //    location services. Visit your ArcGIS Developers Dashboard create a new
+  //    API keys or access an existing API key.
+  const QString apiKey = QStringLiteral("");
+  if (apiKey.isEmpty())
+  {
+    qWarning() << "Use of Esri location services, including basemaps, requires"
                         " you to authenticate with an ArcGIS identity or set the API Key property.";
-      }
-      else
-      {
-          QCoreApplication::instance()->setProperty("Esri.ArcGISRuntime.apiKey", apiKey);
-      }
+  }
+  else
+  {
+    QCoreApplication::instance()->setProperty("Esri.ArcGISRuntime.apiKey", apiKey);
+  }
 
-    // Initialize application view
-    QQuickView view;
-    view.setResizeMode(QQuickView::SizeRootObjectToView);
+  // Initialize application view
+  QQuickView view;
+  view.setResizeMode(QQuickView::SizeRootObjectToView);
 
-    // Add the import Path
-    view.engine()->addImportPath(QDir(QCoreApplication::applicationDirPath()).filePath("qml"));
+  // Add the import Path
+  view.engine()->addImportPath(QDir(QCoreApplication::applicationDirPath()).filePath("qml"));
 
 #ifdef ARCGIS_RUNTIME_IMPORT_PATH_2
-    view.engine()->addImportPath(ARCGIS_RUNTIME_IMPORT_PATH_2);
+  view.engine()->addImportPath(ARCGIS_RUNTIME_IMPORT_PATH_2);
 #endif
 
-    // Set the source
-    view.setSource(QUrl("qrc:/Samples/Layers/BrowseOGCAPIFeatureService/BrowseOGCAPIFeatureService.qml"));
+  // Set the source
+  view.setSource(QUrl("qrc:/Samples/Layers/BrowseOGCAPIFeatureService/BrowseOGCAPIFeatureService.qml"));
 
-    view.show();
+  view.show();
 
-    return app.exec();
+  return app.exec();
 }
