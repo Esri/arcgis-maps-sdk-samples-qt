5b9300e900270df5960d1acf5ddf1e40a891cf8d
Gela Malek Pour <GMalekpour@esri.com>
2021-09-13 08:39:29 -0700

    Instantiate new project

---
+++ ArcGISRuntimeSDKQt_CppSamples/Layers/Query_OGC/Query_OGC.cpp
@@ -0,0 +1,59 @@
+// [WriteFile Name=Query_OGC, Category=Layers]
+// [Legal]
+// Copyright 2021 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#ifdef PCH_BUILD
+#include "pch.hpp"
+#endif // PCH_BUILD
+
+#include "Query_OGC.h"
+
+#include "Map.h"
+#include "MapQuickView.h"
+
+using namespace Esri::ArcGISRuntime;
+
+Query_OGC::Query_OGC(QObject* parent /* = nullptr */):
+    QObject(parent),
+    m_map(new Map(BasemapStyle::ArcGISTopographic, this))
+{
+
+}
+
+Query_OGC::~Query_OGC() = default;
+
+void Query_OGC::init()
+{
+    // Register the map view for QML
+    qmlRegisterType<MapQuickView>("Esri.Samples", 1, 0, "MapView");
+    qmlRegisterType<Query_OGC>("Esri.Samples", 1, 0, "Query_OGCSample");
+}
+
+MapQuickView* Query_OGC::mapView() const
+{
+    return m_mapView;
+}
+
+// Set the view (created in QML)
+void Query_OGC::setMapView(MapQuickView* mapView)
+{
+    if (!mapView || mapView == m_mapView)
+        return;
+
+    m_mapView = mapView;
+    m_mapView->setMap(m_map);
+
+    emit mapViewChanged();
+}
