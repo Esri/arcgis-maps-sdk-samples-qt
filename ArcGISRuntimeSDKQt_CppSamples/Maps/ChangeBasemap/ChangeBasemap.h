8796d54b6422953cda8b437eb88da06979beaf78
AndrewBladon <abladon@esri.com>
2021-09-23 11:55:58 +0100

    Add new files for c++ sample from sample template

---
+++ ArcGISRuntimeSDKQt_CppSamples/Maps/ChangeBasemap/ChangeBasemap.h
@@ -0,0 +1,54 @@
+// [WriteFile Name=ChangeBasemap, Category=Maps]
+// [Legal]
+// Copyright 2021 Esri.
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// [Legal]
+
+#ifndef CHANGEBASEMAP_H
+#define CHANGEBASEMAP_H
+
+namespace Esri
+{
+namespace ArcGISRuntime
+{
+class Map;
+class MapQuickView;
+}
+}
+
+#include <QObject>
+
+class ChangeBasemap : public QObject
+{
+  Q_OBJECT
+
+  Q_PROPERTY(Esri::ArcGISRuntime::MapQuickView* mapView READ mapView WRITE setMapView NOTIFY mapViewChanged)
+
+public:
+  explicit ChangeBasemap(QObject* parent = nullptr);
+  ~ChangeBasemap();
+
+  static void init();
+
+signals:
+  void mapViewChanged();
+
+private:
+  Esri::ArcGISRuntime::MapQuickView* mapView() const;
+  void setMapView(Esri::ArcGISRuntime::MapQuickView* mapView);
+
+  Esri::ArcGISRuntime::Map* m_map = nullptr;
+  Esri::ArcGISRuntime::MapQuickView* m_mapView = nullptr;
+};
+
+#endif // CHANGEBASEMAP_H
