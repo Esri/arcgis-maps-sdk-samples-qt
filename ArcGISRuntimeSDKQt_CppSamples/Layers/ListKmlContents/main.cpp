edf82c7ce60800f1a2ed0f8c2e4d1d6d56f95e83
James Ballard <JamesMBallard@users.noreply.github.com>
2021-10-22 10:00:35 -0700

    Avoid clazy warning with QStringLiteral (#1324)
    Standardize API Key warning message formatting.
    Also avoid clazy warning with empty QStringLiteral.

--- ArcGISRuntimeSDKQt_CppSamples/Layers/ListKmlContents/main.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/Layers/ListKmlContents/main.cpp
@@ -44,7 +44,7 @@ int main(int argc, char *argv[])
   // 2. API key: A permanent key that gives your application access to Esri
   //    location services. Visit your ArcGIS Developers Dashboard create a new
   //    API keys or access an existing API key.
-  const QString apiKey = QStringLiteral("");
+  const QString apiKey = QString("");
   if (apiKey.isEmpty())
   {
     qWarning() << "Use of Esri location services, including basemaps, requires" <<
