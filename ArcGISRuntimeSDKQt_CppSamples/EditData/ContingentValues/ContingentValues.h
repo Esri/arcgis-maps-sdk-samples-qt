3fd015240e216a3d0deb64665a802545f74da19c
Tanner Yould <tyould@esri.com>
2022-03-01 11:56:23 -0800

    Tanneryould/contingent values fixes (#1359)
    * Update copyright date to 2022
    
    * Specify which geodatabase table

--- ArcGISRuntimeSDKQt_CppSamples/EditData/ContingentValues/ContingentValues.h
+++ ArcGISRuntimeSDKQt_CppSamples/EditData/ContingentValues/ContingentValues.h
@@ -1,6 +1,6 @@
 // [WriteFile Name=ContingentValues, Category=EditData]
 // [Legal]
-// Copyright 2021 Esri.
+// Copyright 2022 Esri.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
