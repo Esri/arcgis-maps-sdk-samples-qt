dd149ed0fb6518c1fd1e3db3d9e60a790437daa3
Gela Malek Pour <GMalekpour@esri.com>
2022-08-07 21:55:11 -0700

    Add QML sample

---
+++ ArcGISRuntimeSDKQt_QMLSamples/Routing/NavigateARouteWithRerouting/NavigateRouteSpeaker.h
@@ -0,0 +1,22 @@
+#ifndef NAVIGATEROUTESPEAKER_H
+#define NAVIGATEROUTESPEAKER_H
+
+#include <QObject>
+
+class QTextToSpeech;
+class NavigateRouteSpeaker : public QObject
+{
+  Q_OBJECT
+
+public:
+  explicit NavigateRouteSpeaker(QObject* parent = nullptr);
+  ~NavigateRouteSpeaker();
+
+  Q_INVOKABLE void textToSpeech(const QString& text);
+  Q_INVOKABLE bool textToSpeechEngineReady() const;
+
+private:
+  QTextToSpeech* m_speaker = nullptr;
+};
+
+#endif // NAVIGATEROUTESPEAKER_H
