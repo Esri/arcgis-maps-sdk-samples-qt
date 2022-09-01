f73fbd02dee1aa478acefd1da627ac60ee2092ff
Tanner Yould <tyould@esri.com>
2022-08-31 17:05:37 -0700

    Further TTS removal

--- ArcGISRuntimeSDKQt_QMLSamples/Routing/NavigateRoute/main.cpp
+++ ArcGISRuntimeSDKQt_QMLSamples/Routing/NavigateRoute/main.cpp
@@ -18,7 +18,7 @@
 #include <QDir>
 #include <QQmlEngine>
 
-#include "NavigateRouteSpeaker.h"
+// #include "NavigateRouteSpeaker.h"
 
 int main(int argc, char *argv[])
 {
@@ -48,7 +48,7 @@ int main(int argc, char *argv[])
   view.setResizeMode(QQuickView::SizeRootObjectToView);
 
   // Register the C++ NavigateRouteSpeaker class
-  qmlRegisterType<NavigateRouteSpeaker>("Esri.samples", 1, 0, "NavigateRouteSpeaker");
+  // qmlRegisterType<NavigateRouteSpeaker>("Esri.samples", 1, 0, "NavigateRouteSpeaker");
 
   // Add the import Path
   view.engine()->addImportPath(QDir(QCoreApplication::applicationDirPath()).filePath("qml"));
