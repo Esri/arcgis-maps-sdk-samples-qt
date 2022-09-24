6b84ba951a732ea72a45fb00758b9df7a35f7e25
Annika Oeth <112196959+annika-oeth@users.noreply.github.com>
2022-09-23 17:39:25 -0700

    fixed spacing

--- ArcGISRuntimeSDKQt_CppSamples/Search/FindPlace/main.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/Search/FindPlace/main.cpp
@@ -56,9 +56,9 @@ int main(int argc, char *argv[])
     FindPlace::init();
 
     // Enable OpenGL
-  qputenv("QSG_RHI_BACKEND", "opengl");
+    qputenv("QSG_RHI_BACKEND", "opengl");
 
-  // Initialize application view
+    // Initialize application view
     QQuickView view;
     view.setResizeMode(QQuickView::SizeRootObjectToView);
 
