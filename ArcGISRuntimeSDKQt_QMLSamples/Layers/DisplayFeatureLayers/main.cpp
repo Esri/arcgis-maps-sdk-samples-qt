4d8dc2d4c7f405b065ed5ea4626987c09400b457
Tanner Yould <tyould@esri.com>
2023-01-10 13:39:25 -0800

    Remove trade secrets header

--- ArcGISRuntimeSDKQt_QMLSamples/Layers/DisplayFeatureLayers/main.cpp
+++ ArcGISRuntimeSDKQt_QMLSamples/Layers/DisplayFeatureLayers/main.cpp
@@ -1,18 +1,6 @@
-
-// TRADE SECRETS: ESRI PROPRIETARY AND CONFIDENTIAL
-// Unpublished material - all rights reserved under the
-// Copyright Laws of the United States and applicable international
-// laws, treaties, and conventions.
-//
-// For additional information, contact:
-// Environmental Systems Research Institute, Inc.
-// Attn: Contracts and Legal Services Department
-// 380 New York Street
-// Redlands, California, 92373
-// USA
-//
-// email: contracts@esri.com
-/// \file main.cpp
+// [WriteFile Name=DisplayFeatureLayers, Category=Layers]
+// [Legal]
+// Copyright 2022 Esri.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -24,6 +12,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+// [Legal]
 
 #include <QSettings>
 #include <QGuiApplication>
