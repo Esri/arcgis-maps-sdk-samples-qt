8138d598f72085772e5c979257abf3ddb70cd3c1
Tanner Yould <tyould@esri.com>
2023-03-10 16:28:32 -0800

    connections cleanups

--- ArcGISRuntimeSDKQt_CppSamples/Layers/ExportVectorTiles/ExportVectorTiles.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/Layers/ExportVectorTiles/ExportVectorTiles.cpp
@@ -1,6 +1,18 @@
-// [WriteFile Name=ExportVectorTiles, Category=Layers]
-// [Legal]
-// Copyright 2022 Esri.
+// COPYRIGHT 2023 ESRI
+// TRADE SECRETS: ESRI PROPRIETARY AND CONFIDENTIAL
+// Unpublished material - all rights reserved under the
+// Copyright Laws of the United States and applicable international
+// laws, treaties, and conventions.
+//
+// For additional information, contact:
+// Environmental Systems Research Institute, Inc.
+// Attn: Contracts and Legal Services Department
+// 380 New York Street
+// Redlands, California, 92373
+// USA
+//
+// email: contracts@esri.com
+/// \file ExportVectorTiles.cpp
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -153,7 +165,7 @@ void ExportVectorTiles::startExport(double xSW, double ySW, double xNE, double y
       emit jobStatusChanged();
     });
 
-    connect(m_exportJob, &Job::cancelAsyncCompleted, this, [this](QUuid taskId, bool succeeded)
+    connect(m_exportJob, &Job::cancelAsyncCompleted, this, [this](QUuid, bool succeeded)
     {
       emit jobCancelDone(succeeded);
     });
--- ArcGISRuntimeSDKQt_CppSamples_Widgets/Maps/MapLoaded/MapLoaded.cpp
+++ ArcGISRuntimeSDKQt_CppSamples_Widgets/Maps/MapLoaded/MapLoaded.cpp
@@ -1,6 +1,18 @@
-// [WriteFile Name=MapLoaded, Category=Maps]
-// [Legal]
-// Copyright 2015 Esri.
+// COPYRIGHT 2023 ESRI
+// TRADE SECRETS: ESRI PROPRIETARY AND CONFIDENTIAL
+// Unpublished material - all rights reserved under the
+// Copyright Laws of the United States and applicable international
+// laws, treaties, and conventions.
+//
+// For additional information, contact:
+// Environmental Systems Research Institute, Inc.
+// Attn: Contracts and Legal Services Department
+// 380 New York Street
+// Redlands, California, 92373
+// USA
+//
+// email: contracts@esri.com
+/// \file MapLoaded.cpp
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -36,7 +48,7 @@ MapLoaded::MapLoaded(QWidget* parent) :
     m_loadStatus= new QLabel("", this);
 
     // Map Load Status changed lambda
-    connect(m_map, &Esri::ArcGISRuntime::Map::loadStatusChanged, this, [this](Esri::ArcGISRuntime::LoadStatus loadStatus)
+    connect(m_map, &Map::loadStatusChanged, this, [this](LoadStatus loadStatus)
     {
         switch (loadStatus)
         {
