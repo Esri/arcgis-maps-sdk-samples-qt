be91028ed5aed09e2f192e72ec360df62e43103d
bri12415 <bnoble@esri.com>
2023-09-21 14:14:15 -0700

    remove comment

--- ArcGISRuntimeSDKQt_CppSamples/Scenes/SyncMapViewSceneView/SyncMapViewSceneView.cpp
+++ ArcGISRuntimeSDKQt_CppSamples/Scenes/SyncMapViewSceneView/SyncMapViewSceneView.cpp
@@ -95,7 +95,7 @@ void SyncMapViewSceneView::setMapView(MapQuickView* mapView)
   {
     if (m_sceneView && m_mapView->isNavigating())
       m_sceneView->setViewpoint(m_mapView->currentViewpoint(ViewpointType::CenterAndScale), 0);
-  }); // lambda-unique-connection: Finds usages of Qt::UniqueConnection when the slot is a functor, lambda or non-member function. That connect() overload does not support Qt::UniqueConnection.
+  });
 
   emit mapViewChanged();
 }
