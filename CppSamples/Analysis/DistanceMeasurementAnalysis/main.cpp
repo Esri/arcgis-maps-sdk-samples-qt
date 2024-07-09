36fb5bbec928e0b51ea2b395bf9dbfe032a7928e
jared <jcaccamo@esri.com>
2024-07-09 11:25:59 -0700

    helper removed.

--- CppSamples/Analysis/DistanceMeasurementAnalysis/main.cpp
+++ CppSamples/Analysis/DistanceMeasurementAnalysis/main.cpp
@@ -82,35 +82,5 @@ int main(int argc, char *argv[])
   return app.exec();
 }
 
-// Use of Esri location services, including basemaps and geocoding,
-// requires authentication using either an ArcGIS identity or an API Key.
-// 1. ArcGIS identity: An ArcGIS named user account that is a member of an
-//    organization in ArcGIS Online or ArcGIS Enterprise.
-// 2. API key: API key: a permanent key that grants access to
-//    location services and premium content in your applications.
-//    Visit your ArcGIS Developers Dashboard to create a new
-//    API key or access an existing API key.
 
-void setAPIKey(const QGuiApplication& app, QString apiKey)
-{
-  if (apiKey.isEmpty())
-  {
-    // Try parsing API key from command line argument, which uses the following syntax "-k <apiKey>".
-    QCommandLineParser cmdParser;
-    QCommandLineOption apiKeyArgument(QStringList{"k", "api"}, "The API Key property used to access Esri location services", "apiKeyInput");
-    cmdParser.addOption(apiKeyArgument);
-    cmdParser.process(app);
-
-    apiKey = cmdParser.value(apiKeyArgument);
-
-    if (apiKey.isEmpty())
-    {
-      qWarning() << "Use of Esri location services, including basemaps, requires" <<
-                    "you to authenticate with an ArcGIS identity or set the API Key property.";
-      return;
-    }
-  }
-
-  Esri::ArcGISRuntime::ArcGISRuntimeEnvironment::setApiKey(apiKey);
-}
 
