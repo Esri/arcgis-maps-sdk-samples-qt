08382f85af38483677c606da7ce2c3596e32e493
Tanner Yould <tyould@esri.com>
2024-10-23 15:22:00 -0700

    Update ShowGrid

--- CppSamples/DisplayInformation/ShowGrid/ShowGrid.cpp
+++ CppSamples/DisplayInformation/ShowGrid/ShowGrid.cpp
@@ -1,12 +1,12 @@
-// [WriteFile Name=DisplayGrid, Category=DisplayInformation]
+// [WriteFile Name=ShowGrid, Category=DisplayInformation]
 // [Legal]
-// Copyright 2016 Esri.
-//
+// Copyright 2024 Esri.
+
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 // http://www.apache.org/licenses/LICENSE-2.0
-//
+
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -18,298 +18,256 @@
 #include "pch.hpp"
 #endif // PCH_BUILD
 
-// sample headers
-#include "DisplayGrid.h"
+#include "ShowGrid.h"
 
-// ArcGIS Maps SDK headers
-#include "Grid.h"
+#include "ArcGISTiledElevationSource.h"
+#include "ElevationSourceListModel.h"
 #include "LatitudeLongitudeGrid.h"
-#include "MGRSGrid.h"
 #include "Map.h"
-#include "MapQuickView.h"
 #include "MapTypes.h"
+#include "MapQuickView.h"
 #include "MapViewTypes.h"
+#include "MGRSGrid.h"
 #include "Point.h"
+#include "Scene.h"
+#include "SceneQuickView.h"
 #include "SimpleLineSymbol.h"
 #include "SpatialReference.h"
-#include "SymbolTypes.h"
+#include "Surface.h"
 #include "TextSymbol.h"
 #include "USNGGrid.h"
 #include "UTMGrid.h"
 #include "Viewpoint.h"
 
+#include <QFuture>
+
 using namespace Esri::ArcGISRuntime;
 
-const QString DisplayGrid::s_utmGrid = QStringLiteral("UTM");
-const QString DisplayGrid::s_usngGrid = QStringLiteral("USNG");
-const QString DisplayGrid::s_latlonGrid = QStringLiteral("LatLon");
-const QString DisplayGrid::s_mgrsGrid = QStringLiteral("MGRS");
-const QString DisplayGrid::s_ddFormat = QStringLiteral("Decimal degrees");
-const QString DisplayGrid::s_dmsFormat = QStringLiteral( "Degrees minutes seconds");
-const QString DisplayGrid::s_geographicPosition = QStringLiteral("Geographic");
-const QString DisplayGrid::s_bottomLeftPosition = QStringLiteral("Bottom left");
-const QString DisplayGrid::s_bottomRightPosition = QStringLiteral("Bottom right");
-const QString DisplayGrid::s_topLeftPosition = QStringLiteral("Top left");
-const QString DisplayGrid::s_topRightPosition = QStringLiteral("Top right");
-const QString DisplayGrid::s_centerPosition = QStringLiteral("Center");
-const QString DisplayGrid::s_allSidesPosition = QStringLiteral("All sides");
-
-DisplayGrid::DisplayGrid(QQuickItem* parent /* = nullptr */):
-  QQuickItem(parent)
+ShowGrid::ShowGrid(QObject* parent /* = nullptr */):
+  QObject(parent),
+  m_map(new Map(BasemapStyle::ArcGISImagery, this)),
+  m_scene(new Scene(BasemapStyle::ArcGISImagery, this))
 {
-}
+  // create a new elevation source from Terrain3D REST service
+  ArcGISTiledElevationSource* elevationSource = new ArcGISTiledElevationSource(
+        QUrl("https://elevation3d.arcgis.com/arcgis/rest/services/WorldElevation3D/Terrain3D/ImageServer"), this);
 
-DisplayGrid::~DisplayGrid() = default;
+  // add the elevation source to the scene to display elevation
+  m_scene->baseSurface()->elevationSources()->append(elevationSource);
 
-void DisplayGrid::init()
-{
-  qmlRegisterType<MapQuickView>("Esri.Samples", 1, 0, "MapView");
-  qmlRegisterType<DisplayGrid>("Esri.Samples", 1, 0, "DisplayGridSample");
-}
+  // Set the sample's initial GeoView type to MapView
+  m_currentViewType = s_mapView;
 
-void DisplayGrid::componentComplete()
-{
-  QQuickItem::componentComplete();
-
-  // find QML MapView component
-  m_mapView->setWrapAroundMode(WrapAroundMode::Disabled);
-
-  // Create a map using the imagery basemap
-  m_map = new Map(BasemapStyle::ArcGISImageryStandard, this);
-  double targetScale = 6450785;
+  // Set the initial viewpoint of the map
+  constexpr double targetScale = 6450785;
   m_map->setInitialViewpoint(Viewpoint(Point(-10336141.70018318, 5418213.05332071, SpatialReference::webMercator()), targetScale));
 
-  // Set map to map view
-  m_mapView->setMap(m_map);
+  // Set the intial grid to LatitudeLongitudeGrid
+  m_grid = new LatitudeLongitudeGrid(this);
 }
 
-// change the grid type
-void DisplayGrid::changeGrid(const QString& gridType)
+ShowGrid::~ShowGrid() = default;
+
+void ShowGrid::init()
 {
-  if (gridType == latlonGrid())
-  {
-    //! [DisplayGrid Set_LatLon_Grid_Cpp]
-    // create a grid for showing Latitude and Longitude (Meridians and Parallels)
-    LatitudeLongitudeGrid* latlonGrid = new LatitudeLongitudeGrid(this);
-    m_mapView->setGrid(latlonGrid);
-    //! [DisplayGrid Set_LatLon_Grid_Cpp]
-  }
-  else if (gridType == utmGrid())
-  {
-    UTMGrid* utmGrid = new UTMGrid(this);
-    m_mapView->setGrid(utmGrid);
-  }
-  else if (gridType == usngGrid())
-  {
-    USNGGrid* utmGrid = new USNGGrid(this);
-    m_mapView->setGrid(utmGrid);
-  }
-  else if (gridType == mgrsGrid())
-  {
-    MGRSGrid* utmGrid = new MGRSGrid(this);
-    m_mapView->setGrid(utmGrid);
-  }
-
-  // apply any styling that has been set
-  changeGridColor(currentGridColor());
-  changeLabelColor(currentLabelColor());
-  changeLabelFormat(currentLabelFormat());
-  changeLabelPosition(currentLabelPosition());
-  setGridLabelVisibility(gridVisibility());
-  setGridVisibility(gridVisibility());
+  // Register classes for QML
+  qmlRegisterType<MapQuickView>("Esri.Samples", 1, 0, "MapView");
+  qmlRegisterType<SceneQuickView>("Esri.Samples", 1, 0, "SceneView");
+  qmlRegisterType<ShowGrid>("Esri.Samples", 1, 0, "ShowGridSample");
 }
 
-// change the grid color
-void DisplayGrid::changeGridColor(const QString& color)
-{
-  if (m_mapView->grid())
-  {
-    // find the number of resolution levels in the grid
-    int gridLevels = m_mapView->grid()->levelCount();
-
-    //! [DisplayGrid Set_Grid_Lines_Cpp]
-    for (int level = 0; level < gridLevels; level++)
-    {
-      const float width = 1 + level;
-      SimpleLineSymbol* lineSym = new SimpleLineSymbol(SimpleLineSymbolStyle::Solid, QColor(color), width, this);
-      m_mapView->grid()->setLineSymbol(level, lineSym);
-    }
-    //! [DisplayGrid Set_Grid_Lines_Cpp]
-  }
-}
-
-// change the grid label color
-void DisplayGrid::changeLabelColor(const QString& color)
-{
-  if (m_mapView->grid())
-  {
-    //! [DisplayGrid Grid_Levels_Cpp]
-    // find the number of resolution levels in the grid
-    int gridLevels = m_mapView->grid()->levelCount();
-    //! [DisplayGrid Grid_Levels_Cpp]
-
-    //! [DisplayGrid Set_Grid_Labels_Cpp]
-    for (int level = 0; level < gridLevels; level++)
-    {
-      constexpr float size = 14.0f;
-      TextSymbol* textSym = new TextSymbol("text", QColor(color), size, HorizontalAlignment::Left, VerticalAlignment::Bottom, this);
-      textSym->setHaloColor(QColor("white"));
-      textSym->setHaloWidth(2.0f + level);
-      m_mapView->grid()->setTextSymbol(level, textSym);
-    }
-    //! [DisplayGrid Set_Grid_Labels_Cpp]
-  }
-}
-
-// change the grid label format
-void DisplayGrid::changeLabelFormat(const QString& format)
-{
-  if (m_mapView->grid())
-  {
-    if (m_mapView->grid()->gridType() == GridType::LatitudeLongitudeGrid)
-    {
-      if (format == ddFormat())
-      {
-        // format the labels to display in decimal degrees
-        static_cast<LatitudeLongitudeGrid*>(m_mapView->grid())->setLabelFormat(LatitudeLongitudeGridLabelFormat::DecimalDegrees);
-      }
-      else if (format == dmsFormat())
-      {
-        //! [DisplayGrid Set_Label_Format_Cpp]
-        // format the labels to display in degrees, minutes and seconds
-        static_cast<LatitudeLongitudeGrid*>(m_mapView->grid())->setLabelFormat(LatitudeLongitudeGridLabelFormat::DegreesMinutesSeconds);
-        //! [DisplayGrid Set_Label_Format_Cpp]
-      }
-    }
-  }
-}
-
-// change the grid label placement
-void DisplayGrid::changeLabelPosition(const QString& position)
-{
-  if (m_mapView->grid())
-  {
-    if (position == geographicPosition())
-    {
-      //! [DisplayGrid Set_Label_Placement_Geographic_Cpp]
-      // update the label positioning to geographic
-      m_mapView->grid()->setLabelPosition(GridLabelPosition::Geographic);
-      //! [DisplayGrid Set_Label_Placement_Geographic_Cpp]
-    }
-    else if (position == bottomLeftPosition())
-    {
-      // update the label positioning to bottom left
-      m_mapView->grid()->setLabelPosition(GridLabelPosition::BottomLeft);
-    }
-    else if (position == bottomRightPosition())
-    {
-      // update the label positioning to bottom right
-      m_mapView->grid()->setLabelPosition(GridLabelPosition::BottomRight);
-    }
-    else if (position == topLeftPosition())
-    {
-      // update the label positioning to top left
-      m_mapView->grid()->setLabelPosition(GridLabelPosition::TopLeft);
-    }
-    else if (position == topRightPosition())
-    {
-      //! [DisplayGrid Set_Label_Placement_Top_Right_Cpp]
-      // update the label positioning to top right
-      m_mapView->grid()->setLabelPosition(GridLabelPosition::TopRight);
-      //! [DisplayGrid Set_Label_Placement_Top_Right_Cpp]
-    }
-    else if (position == centerPosition())
-    {
-      // update the label positioning to center
-      m_mapView->grid()->setLabelPosition(GridLabelPosition::Center);
-    }
-    else if (position == allSidesPosition())
-    {
-      // update the label positioning to all sides
-      m_mapView->grid()->setLabelPosition(GridLabelPosition::AllSides);
-    }
-  }
-}
-
-MapQuickView* DisplayGrid::mapQuickView() const
+MapQuickView* ShowGrid::mapView() const
 {
   return m_mapView;
 }
 
-void DisplayGrid::setMapQuickView(MapQuickView* mapView)
+// Set the view (created in QML)
+void ShowGrid::setMapView(MapQuickView* mapView)
 {
+  if (!mapView || mapView == m_mapView)
+    return;
+
   m_mapView = mapView;
-  emit mapQuickViewChanged();
+  m_mapView->setMap(m_map);
+
+  m_mapView->setGrid(m_grid);
+
+  emit viewChanged();
 }
 
-QString DisplayGrid::currentGridColor() const
+SceneQuickView* ShowGrid::sceneView() const
 {
-  return m_currentGridColor;
+  return m_sceneView;
 }
 
-void DisplayGrid::setCurrentGridColor(const QString& gridColor)
+// Set the view (created in QML)
+void ShowGrid::setSceneView(SceneQuickView* sceneView)
 {
-  m_currentGridColor = gridColor;
-  changeGridColor(m_currentGridColor);
+  if (!sceneView || sceneView == m_sceneView)
+    return;
+
+  m_sceneView = sceneView;
+  m_sceneView->setArcGISScene(m_scene);
+
+  // Set the SceneView initially to be invisible
+  m_sceneView->setVisible(false);
+
+  emit viewChanged();
 }
 
-QString DisplayGrid::currentLabelColor() const
+
+void ShowGrid::setViewType(const QString& viewType)
 {
-  return m_currentLabelColor;
+  if (m_currentViewType == viewType)
+    return;
+
+  m_currentViewType = viewType;
+
+  if (viewType == s_mapView)
+  {
+    m_mapView->setViewpointAsync(m_sceneView->currentViewpoint(ViewpointType::BoundingGeometry), 0);
+
+    m_mapView->setVisible(true);
+    m_sceneView->setVisible(false);
+
+    m_sceneView->setGrid(nullptr);
+    m_mapView->setGrid(m_grid);
+  }
+  else if (viewType == s_sceneView)
+  {
+    m_sceneView->setViewpointAsync(m_mapView->currentViewpoint(ViewpointType::BoundingGeometry), 0);
+
+    m_mapView->setVisible(false);
+    m_sceneView->setVisible(true);
+
+    m_mapView->setGrid(nullptr);
+    m_sceneView->setGrid(m_grid);
+  }
+
+  emit propertiesChanged();
 }
 
-void DisplayGrid::setCurrentLabelColor(const QString& labelColor)
+void ShowGrid::setGridType(const QString& gridType)
+{
+  if (m_currentGridType == gridType)
+    return;
+
+  m_currentGridType = gridType;
+
+  if (m_grid)
+  {
+    delete m_grid;
+    m_grid = nullptr;
+  }
+
+  // Create a new Grid of the selected type
+  if (gridType == s_latLong)
+    m_grid = new LatitudeLongitudeGrid(this);
+  else if (gridType == s_mgrs)
+    m_grid = new MGRSGrid(this);
+  else if (gridType == s_utm)
+    m_grid = new UTMGrid(this);
+  else if (gridType == s_usng)
+    m_grid = new USNGGrid(this);
+
+  // Set the grid on the current view
+  if (m_mapView->isVisible())
+    m_mapView->setGrid(m_grid);
+  else
+    m_sceneView->setGrid(m_grid);
+
+  // Set properties from current UI values
+  setGridVisible(m_gridVisible);
+  setLabelsVisible(m_labelsVisible);
+  setLineColor(m_currentLineColor);
+  setLabelColor(m_currentLabelColor);
+  setLabelPosition(m_currentLabelPosition);
+  setLabelFormat(m_currentLabelFormat);
+}
+
+void ShowGrid::setGridVisible(bool visible)
+{
+  if (m_grid->isVisible() == visible)
+    return;
+
+  m_gridVisible = visible;
+  m_grid->setVisible(visible);
+
+  emit propertiesChanged();
+}
+
+void ShowGrid::setLabelsVisible(bool visible)
+{
+  if (m_grid->isLabelsVisible() == visible)
+    return;
+
+  m_labelsVisible = visible;
+  m_grid->setLabelsVisible(visible);
+
+  emit propertiesChanged();
+}
+
+void ShowGrid::setLineColor(const QString& lineColor)
+{  
+  m_currentLineColor = lineColor;
+
+  SimpleLineSymbol* lineSymbol = static_cast<SimpleLineSymbol*>(m_grid->lineSymbol(0));
+  lineSymbol->setColor(lineColor.toLower());
+
+  // Some grids have multiple levels, in this sample we set the same symbol for all levels
+  for (int level = 0; level < m_grid->levelCount(); ++level)
+    m_grid->setLineSymbol(level, lineSymbol);
+
+  emit propertiesChanged();
+}
+
+void ShowGrid::setLabelColor(const QString& labelColor)
 {
   m_currentLabelColor = labelColor;
-  changeLabelColor(m_currentLabelColor);
+
+  TextSymbol* labelSymbol = static_cast<TextSymbol*>(m_grid->textSymbol(0));
+  labelSymbol->setColor(labelColor.toLower());
+
+  // Some grids have multiple levels, in this sample we set the same symbol for all levels
+  for (int level = 0; level < m_grid->levelCount(); ++level)
+    m_grid->setTextSymbol(level, labelSymbol);
+
+  emit propertiesChanged();
 }
 
-QString DisplayGrid::currentLabelFormat() const
-{
-  return m_currentLabelFormat;
-}
-
-void DisplayGrid::setCurrentLabelFormat(const QString& labelFormat)
-{
-  m_currentLabelFormat = labelFormat;
-  changeLabelFormat(m_currentLabelFormat);
-}
-
-QString DisplayGrid::currentLabelPosition() const
-{
-  return m_currentLabelPosition;
-}
-
-void DisplayGrid::setCurrentLabelPosition(const QString& labelPosition)
+void ShowGrid::setLabelPosition(const QString& labelPosition)
 {
   m_currentLabelPosition = labelPosition;
-  changeLabelPosition(m_currentLabelPosition);
+
+  if (labelPosition == s_geographic)
+    m_grid->setLabelPosition(GridLabelPosition::Geographic);
+  else if (labelPosition == s_bottomLeft)
+    m_grid->setLabelPosition(GridLabelPosition::BottomLeft);
+  else if (labelPosition == s_bottomRight)
+    m_grid->setLabelPosition(GridLabelPosition::BottomRight);
+  else if (labelPosition == s_topLeft)
+    m_grid->setLabelPosition(GridLabelPosition::TopLeft);
+  else if (labelPosition == s_topRight)
+    m_grid->setLabelPosition(GridLabelPosition::TopRight);
+  else if (labelPosition == s_center)
+    m_grid->setLabelPosition(GridLabelPosition::Center);
+  else if (labelPosition == s_allSides)
+    m_grid->setLabelPosition(GridLabelPosition::AllSides);
+
+  emit propertiesChanged();
 }
 
-bool DisplayGrid::gridVisibility()
+void ShowGrid::setLabelFormat(const QString& labelFormat)
 {
-  return m_gridVisibility;
-}
+  // Only LatitudeLongitudeGrid supports label formats
+  if (m_grid->gridType() != GridType::LatitudeLongitudeGrid)
+    return;
 
-void DisplayGrid::setGridVisibility(bool visible)
-{
-  m_gridVisibility = visible;
+  m_currentLabelFormat = labelFormat;
+  if (labelFormat == s_decimalDegrees)
+    static_cast<LatitudeLongitudeGrid*>(m_grid)->setLabelFormat(LatitudeLongitudeGridLabelFormat::DecimalDegrees);
+  else if (labelFormat == s_degreesMinutesSeconds)
+    static_cast<LatitudeLongitudeGrid*>(m_grid)->setLabelFormat(LatitudeLongitudeGridLabelFormat::DegreesMinutesSeconds);
 
-  //! [DisplayGrid Grid_Visible_Cpp]
-  m_mapView->grid()->setVisible(m_gridVisibility);
-  //! [DisplayGrid Grid_Visible_Cpp]
-}
-
-bool DisplayGrid::gridLabelVisibility()
-{
-  return m_gridLabelVisibility;
-}
-
-void DisplayGrid::setGridLabelVisibility(bool visible)
-{
-  m_gridLabelVisibility = visible;
-
-  //! [DisplayGrid Label_Visible_Cpp]
-  m_mapView->grid()->setLabelsVisible(m_gridLabelVisibility);
-  //! [DisplayGrid Label_Visible_Cpp]
+  emit propertiesChanged();
 }
--- CppSamples/DisplayInformation/ShowGrid/ShowGrid.h
+++ CppSamples/DisplayInformation/ShowGrid/ShowGrid.h
@@ -1,12 +1,12 @@
-// [WriteFile Name=DisplayGrid, Category=DisplayInformation]
+// [WriteFile Name=ShowGrid, Category=DisplayInformation]
 // [Legal]
-// Copyright 2016 Esri.
-//
+// Copyright 2024 Esri.
+
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 // http://www.apache.org/licenses/LICENSE-2.0
-//
+
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -14,131 +14,119 @@
 // limitations under the License.
 // [Legal]
 
-#ifndef DISPLAYGRID_H
-#define DISPLAYGRID_H
+#ifndef SHOWGRID_H
+#define SHOWGRID_H
 
-// Qt headers
-#include <QQuickItem>
+#include <QObject>
 
 namespace Esri::ArcGISRuntime
 {
-  class Map;
-  class MapQuickView;
+class Grid;
+class Map;
+class MapQuickView;
+class Scene;
+class SceneQuickView;
+class SimpleLineSymbol;
+class TextSymbol;
 }
 
-Q_MOC_INCLUDE("MapQuickView.h")
+Q_MOC_INCLUDE("MapQuickView.h");
+Q_MOC_INCLUDE("SceneQuickView.h");
 
-class DisplayGrid : public QQuickItem
+class ShowGrid : public QObject
 {
   Q_OBJECT
 
-  Q_PROPERTY(Esri::ArcGISRuntime::MapQuickView* mapView READ mapQuickView WRITE setMapQuickView NOTIFY mapQuickViewChanged)
-
-  Q_PROPERTY(QString utmGrid READ utmGrid NOTIFY utmGridChanged)
-  Q_PROPERTY(QString usngGrid READ usngGrid NOTIFY usngGridChanged)
-  Q_PROPERTY(QString latlonGrid READ latlonGrid NOTIFY latlonGridChanged)
-  Q_PROPERTY(QString mgrsGrid READ mgrsGrid NOTIFY mgrsGridChanged)
-  Q_PROPERTY(QString ddFormat READ ddFormat NOTIFY ddFormatChanged)
-  Q_PROPERTY(QString dmsFormat READ dmsFormat NOTIFY dmsFormatChanged)
-  Q_PROPERTY(QString geographicPosition READ geographicPosition NOTIFY geographicPositionChanged)
-  Q_PROPERTY(QString bottomLeftPosition READ bottomLeftPosition NOTIFY bottomLeftPositionChanged)
-  Q_PROPERTY(QString bottomRightPosition READ bottomRightPosition NOTIFY bottomRightPositionChanged)
-  Q_PROPERTY(QString topLeftPosition READ topLeftPosition NOTIFY topLeftPositionChanged)
-  Q_PROPERTY(QString topRightPosition READ topRightPosition NOTIFY topRightPositionChanged)
-  Q_PROPERTY(QString centerPosition READ centerPosition NOTIFY centerPositionChanged)
-  Q_PROPERTY(QString allSidesPosition READ allSidesPosition NOTIFY allSidesPositionChanged)
-  Q_PROPERTY(QString currentGridColor READ currentGridColor WRITE setCurrentGridColor NOTIFY currentGridColorChanged)
-  Q_PROPERTY(QString currentLabelColor READ currentLabelColor WRITE setCurrentLabelColor NOTIFY currentLabelColorChanged)
-  Q_PROPERTY(QString currentLabelFormat READ currentLabelFormat WRITE setCurrentLabelFormat NOTIFY currentLabelFormatChanged)
-  Q_PROPERTY(QString currentLabelPosition READ currentLabelPosition WRITE setCurrentLabelPosition NOTIFY currentLabelPositionChanged)
-  Q_PROPERTY(bool gridVisibility READ gridVisibility WRITE setGridVisibility NOTIFY gridVisibilityChanged)
-  Q_PROPERTY(bool gridLabelVisibility READ gridLabelVisibility WRITE setGridLabelVisibility NOTIFY gridLabelVisibilityChanged)
+  Q_PROPERTY(Esri::ArcGISRuntime::MapQuickView* mapView READ mapView WRITE setMapView NOTIFY viewChanged)
+  Q_PROPERTY(Esri::ArcGISRuntime::SceneQuickView* sceneView READ sceneView WRITE setSceneView NOTIFY viewChanged)
+  Q_PROPERTY(QString currentViewType MEMBER m_currentViewType NOTIFY propertiesChanged)
+  Q_PROPERTY(QStringList viewTypes MEMBER m_viewTypes CONSTANT)
+  Q_PROPERTY(QString currentGridType MEMBER m_currentGridType NOTIFY propertiesChanged)
+  Q_PROPERTY(QStringList gridTypes MEMBER m_gridTypes CONSTANT)
+  Q_PROPERTY(bool gridVisible MEMBER m_gridVisible NOTIFY propertiesChanged)
+  Q_PROPERTY(bool labelsVisible MEMBER m_labelsVisible NOTIFY propertiesChanged)
+  Q_PROPERTY(QString currentLineColor MEMBER m_currentLineColor NOTIFY propertiesChanged)
+  Q_PROPERTY(QStringList lineColors MEMBER m_lineColors CONSTANT)
+  Q_PROPERTY(QString currentLabelColor MEMBER m_currentLabelColor NOTIFY propertiesChanged)
+  Q_PROPERTY(QStringList labelColors MEMBER m_labelColors CONSTANT)
+  Q_PROPERTY(QString currentLabelPosition MEMBER m_currentLabelPosition NOTIFY propertiesChanged)
+  Q_PROPERTY(QStringList labelFormats MEMBER m_labelFormats CONSTANT)
+  Q_PROPERTY(QString currentLabelFormat MEMBER m_currentLabelFormat NOTIFY propertiesChanged)
+  Q_PROPERTY(QStringList labelPositions MEMBER m_labelPositions CONSTANT)
 
 public:
-  explicit DisplayGrid(QQuickItem* parent = nullptr);
-  ~DisplayGrid() override;
+  explicit ShowGrid(QObject* parent = nullptr);
+  ~ShowGrid() override;
 
-  void componentComplete() override;
   static void init();
-  Q_INVOKABLE void changeGrid(const QString& gridType);
-  Q_INVOKABLE void changeGridColor(const QString& color);
-  Q_INVOKABLE void changeLabelColor(const QString& color);
-  Q_INVOKABLE void changeLabelFormat(const QString& format);
-  Q_INVOKABLE void changeLabelPosition(const QString& position);
+
+  Q_INVOKABLE void setGridVisible(bool visible);
+  Q_INVOKABLE void setLabelsVisible(bool visible);
+  Q_INVOKABLE void setViewType(const QString& viewType);
+  Q_INVOKABLE void setGridType(const QString& gridType);
+  Q_INVOKABLE void setLineColor(const QString& lineColor);
+  Q_INVOKABLE void setLabelColor(const QString& labelColor);
+  Q_INVOKABLE void setLabelPosition(const QString& labelPosition);
+  Q_INVOKABLE void setLabelFormat(const QString& labelFormat);
 
 signals:
-  void utmGridChanged();
-  void usngGridChanged();
-  void latlonGridChanged();
-  void mgrsGridChanged();
-  void ddFormatChanged();
-  void dmsFormatChanged();
-  void geographicPositionChanged();
-  void bottomLeftPositionChanged();
-  void bottomRightPositionChanged();
-  void topLeftPositionChanged();
-  void topRightPositionChanged();
-  void centerPositionChanged();
-  void allSidesPositionChanged();
-  void currentGridColorChanged();
-  void currentLabelColorChanged();
-  void currentLabelFormatChanged();
-  void currentLabelPositionChanged();
-  void gridVisibilityChanged();
-  void gridLabelVisibilityChanged();
-  void mapQuickViewChanged();
+  void viewChanged();
+  void propertiesChanged();
 
 private:
-    static const QString utmGrid() { return s_utmGrid; }
-    static const QString usngGrid() { return s_usngGrid; }
-    static const QString latlonGrid() { return s_latlonGrid; }
-    static const QString mgrsGrid() { return s_mgrsGrid; }
-    static const QString ddFormat() { return s_ddFormat; }
-    static const QString dmsFormat() { return s_dmsFormat; }
-    static const QString geographicPosition() { return s_geographicPosition; }
-    static const QString bottomLeftPosition() { return s_bottomLeftPosition; }
-    static const QString bottomRightPosition() { return s_bottomRightPosition; }
-    static const QString topLeftPosition() { return s_topLeftPosition; }
-    static const QString topRightPosition() { return s_topRightPosition; }
-    static const QString centerPosition() { return s_centerPosition; }
-    static const QString allSidesPosition() { return s_allSidesPosition; }
-    Esri::ArcGISRuntime::MapQuickView* mapQuickView() const;
-    void setMapQuickView(Esri::ArcGISRuntime::MapQuickView* mapView);
-    QString currentGridColor() const;
-    void setCurrentGridColor(const QString& gridColor);
-    QString currentLabelColor() const;
-    void setCurrentLabelColor(const QString& labelColor);
-    QString currentLabelFormat() const;
-    void setCurrentLabelFormat(const QString& labelFormat);
-    QString currentLabelPosition() const;
-    void setCurrentLabelPosition(const QString& labelPosition);
-    bool gridVisibility();
-    void setGridVisibility(bool visible);
-    bool gridLabelVisibility();
-    void setGridLabelVisibility(bool visible);
+  // Declare private methods
+  Esri::ArcGISRuntime::MapQuickView* mapView() const;
+  void setMapView(Esri::ArcGISRuntime::MapQuickView* mapView);
+  Esri::ArcGISRuntime::SceneQuickView* sceneView() const;
+  void setSceneView(Esri::ArcGISRuntime::SceneQuickView* sceneView);
 
-private:
+  // Declare private members
+  Esri::ArcGISRuntime::Grid* m_grid = nullptr;
   Esri::ArcGISRuntime::Map* m_map = nullptr;
   Esri::ArcGISRuntime::MapQuickView* m_mapView = nullptr;
-  static const QString s_utmGrid;
-  static const QString s_usngGrid;
-  static const QString s_latlonGrid;
-  static const QString s_mgrsGrid;
-  static const QString s_ddFormat;
-  static const QString s_dmsFormat;
-  static const QString s_geographicPosition;
-  static const QString s_bottomLeftPosition;
-  static const QString s_bottomRightPosition;
-  static const QString s_topLeftPosition;
-  static const QString s_topRightPosition;
-  static const QString s_centerPosition;
-  static const QString s_allSidesPosition;
-  QString m_currentGridColor = QStringLiteral("red");
-  QString m_currentLabelColor = QStringLiteral("black");
-  QString m_currentLabelFormat;
+  Esri::ArcGISRuntime::Scene* m_scene = nullptr;
+  Esri::ArcGISRuntime::SceneQuickView* m_sceneView = nullptr;
+
+  // Properties
+  QString m_currentViewType;
+  QString m_currentGridType;
+  bool m_gridVisible = true;
+  bool m_labelsVisible = true;
+  QString m_currentLineColor;
+  QString m_currentLabelColor;
   QString m_currentLabelPosition;
-  bool m_gridVisibility = true;
-  bool m_gridLabelVisibility = true;
+  QString m_currentLabelFormat;
+
+  // Constants
+  const QString s_mapView = QStringLiteral("MapView");
+  const QString s_sceneView = QStringLiteral("SceneView");
+  const QStringList m_viewTypes = {s_mapView, s_sceneView};
+
+  const QString s_latLong = QStringLiteral("LatLong");
+  const QString s_mgrs = QStringLiteral("MGRS");
+  const QString s_utm = QStringLiteral("UTM");
+  const QString s_usng = QStringLiteral("USNG");
+  const QStringList m_gridTypes = {s_latLong, s_mgrs, s_utm, s_usng};
+
+  const QString s_white = QStringLiteral("White");
+  const QString s_red = QStringLiteral("Red");
+  const QString s_blue = QStringLiteral("Blue");
+  const QString s_black = QStringLiteral("Black");
+  const QStringList m_lineColors = {s_white, s_red, s_blue};
+  const QStringList m_labelColors = {s_black, s_red, s_blue};
+
+  const QString s_geographic = QStringLiteral("Geographic");
+  const QString s_topLeft = QStringLiteral("Top Left");
+  const QString s_topRight = QStringLiteral("Top Right");
+  const QString s_bottomLeft = QStringLiteral("Bottom Left");
+  const QString s_bottomRight = QStringLiteral("Bottom Right");
+  const QString s_center = QStringLiteral("Center");
+  const QString s_allSides = QStringLiteral("All sides");
+  const QStringList m_labelPositions = {s_geographic, s_topLeft, s_topRight, s_bottomLeft, s_bottomRight, s_center, s_allSides};
+
+  const QString s_decimalDegrees = QStringLiteral("Decimal Degrees");
+  const QString s_degreesMinutesSeconds = QStringLiteral("Degrees Minutes Seconds");
+  const QStringList m_labelFormats = {s_decimalDegrees, s_degreesMinutesSeconds};
 };
 
-#endif // DISPLAYGRID_H
+#endif // SHOWGRID_H
