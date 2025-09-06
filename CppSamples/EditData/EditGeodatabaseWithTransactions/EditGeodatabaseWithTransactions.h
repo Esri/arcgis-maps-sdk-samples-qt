b86b7718394be94734a1e71bea2f97ca9f1f8f58
mkaratoor <mkaratoor@esri.com>
2025-09-05 17:14:38 -0700

    Copyright block fixes

--- CppSamples/EditData/EditGeodatabaseWithTransactions/EditGeodatabaseWithTransactions.h
+++ CppSamples/EditData/EditGeodatabaseWithTransactions/EditGeodatabaseWithTransactions.h
@@ -1,3 +1,6 @@
+// [WriteFile Name=EditGeodatabaseWithTransactions, Category=EditData]
+// [Legal]
+// Copyright 2025 Esri.
 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
