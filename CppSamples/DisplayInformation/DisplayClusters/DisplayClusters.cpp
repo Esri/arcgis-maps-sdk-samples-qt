e74616567c2aa561e30494cf674d3ca0f1895ca0
Ajith CP <achalliyil@esri.com>
2025-09-09 16:53:32 -0700

    Removed unwanted headers

--- CppSamples/DisplayInformation/DisplayClusters/DisplayClusters.cpp
+++ CppSamples/DisplayInformation/DisplayClusters/DisplayClusters.cpp
@@ -1,19 +1,3 @@
-// [WriteFile Name=DisplayClusters, Category=DisplayInformation]
-// [Legal]
-// Copyright 2023 Esri.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// [Legal]
-
 #ifdef PCH_BUILD
 #include "pch.hpp"
 #endif // PCH_BUILD
@@ -36,9 +20,7 @@
 #include "MapTypes.h"
 #include "Point.h"
 #include "Popup.h"
-#include "PopupDefinition.h"
 #include "PopupElement.h"
-#include "PopupExpression.h"
 #include "PopupExpressionEvaluation.h"
 #include "PopupTypes.h"
 #include "PortalItem.h"
