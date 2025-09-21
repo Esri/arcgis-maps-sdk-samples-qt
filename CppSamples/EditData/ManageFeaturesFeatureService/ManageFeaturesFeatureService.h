bb782176ef0bf06673cccd013d93a60413490678
Jared Caccamo <jcaccamo@esri.com>
2025-09-21 08:11:30 -0700

    fixing broken samples build on windows. enum class needs to be public.

--- CppSamples/EditData/ManageFeaturesFeatureService/ManageFeaturesFeatureService.h
+++ CppSamples/EditData/ManageFeaturesFeatureService/ManageFeaturesFeatureService.h
@@ -1,18 +1,3 @@
-// [WriteFile Name=ManageFeaturesFeatureService, Category=EditData]
-// [Legal]
-// Copyright 2025 Esri.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-// [Legal]
 
 #ifndef MANAGEFEATURESFEATURESERVICE_H
 #define MANAGEFEATURESFEATURESERVICE_H
@@ -40,6 +25,10 @@ class ManageFeaturesFeatureService : public QQuickItem
 {
   Q_OBJECT
 
+  Q_PROPERTY(QString featureType READ featureType NOTIFY featureTypeChanged)
+  Q_PROPERTY(OperationMode operationMode READ operationMode WRITE setOperationMode NOTIFY operationModeChanged)
+
+public:
   enum class OperationMode
   {
     AddFeatures,
@@ -49,10 +38,6 @@ class ManageFeaturesFeatureService : public QQuickItem
   };
   Q_ENUM(OperationMode)
 
-  Q_PROPERTY(QString featureType READ featureType NOTIFY featureTypeChanged)
-  Q_PROPERTY(OperationMode operationMode READ operationMode WRITE setOperationMode NOTIFY operationModeChanged)
-
-public:
   explicit ManageFeaturesFeatureService(QQuickItem* parent = nullptr);
   ~ManageFeaturesFeatureService() override;
 
